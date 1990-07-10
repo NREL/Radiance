e085dda88816b5e6c8b9bc1c7283c730b6a0ef86
Gregory J. Ward <gregoryjward@gmail.com>
1990-07-10 18:06:45 +0000

    Initial revision

---
+++ src/rt/newsconstants.h
@@ -0,0 +1,31 @@
+#define textareaheight 100
+#define textbottom 5
+#define thefont /Times-Bold
+#define fontheight 15
+#define textbackgroundRED .80
+#define textbackgroundGREEN .85
+#define textbackgroundBLUE .75
+#define textcursor (_)
+#define textshadowgray .85
+#define minfrac .1
+#define gammacorrection 2.12
+
+#define inputbuffer nws_inputbuffer
+#define pos nws_pos
+#define gamma nws_gamma
+
+#define painter nws_painter
+#define printer nws_printer
+#define getinput nws_getinput
+#define initcanvas cps_initcanvas
+#define box cps_box
+#define cleanup cps_cleanup
+#define printout cps_printout
+#define linefeed cps_linefeed
+#define getclick cps_getclick
+#define isready cps_isready
+#define startcomin cps_startcomin
+#define endcomin cps_endcomin
+#define getkey cps_getkey
+#define delete cps_delete
+
