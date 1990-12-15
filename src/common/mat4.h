ef3de6a18ec196a0806101c80178387f28b9bcbe
greg <greg>
1990-12-15 14:18:18 +0000

    Initial revision

---
+++ src/common/mat4.h
@@ -0,0 +1,18 @@
+/* Copyright (c) 1990 Regents of the University of California */
+
+/* SCCSid "$SunId$ LBL" */
+
+/*
+ * Definitions for 4x4 matrix operations
+ */
+
+#include  "fvect.h"
+
+typedef double  MAT4[4][4];
+
+#ifdef  BSD
+#define  copymat4(m4a,m4b)	bcopy((char *)m4b,(char *)m4a,sizeof(MAT4))
+#else
+#define  copymat4(m4a,m4b)	(void)memcpy((char *)m4a,(char *)m4b,sizeof(MAT4))
+extern char  *memcpy();
+#endif
