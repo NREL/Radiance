6d48235c7005119d980c516a20beff6474969e8e
greg <greg>
1991-03-14 17:37:44 +0000

    Initial revision

---
+++ src/util/setscan.h
@@ -0,0 +1,10 @@
+/* Copyright (c) 1991 Regents of the University of California */
+
+/* SCCSid "$SunId$ LBL" */
+
+/*
+ * Defines for programs using setscan()
+ */
+
+#define  ANGLE		short
+#define  AEND		(-1)
