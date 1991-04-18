50376d6e2c2b311378c2790175450bafce47a0e9
Gregory J. Ward <gregoryjward@gmail.com>
1991-04-18 08:05:57 +0000

    added variables for luminance in and out, xres, yres, nfiles

--- src/px/pcomb.c
+++ src/px/pcomb.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 1989 Regents of the University of California */
+/* Copyright (c) 1991 Regents of the University of California */
 
 #ifndef lint
 static char SCCSid[] = "$SunId$ LBL";
@@ -31,7 +31,12 @@ int	nfiles;				/* number of input files */
 
 char	*vcolin[3] = {"ri", "gi", "bi"};
 char	*vcolout[3] = {"ro", "go", "bo"};
+#define	vbrtin		"li"
+#define vbrtout		"lo"
 
+#define vnfiles		"nfiles"
+#define vxres		"xres"
+#define vyres		"yres"
 #define vxpos		"x"
 #define vypos		"y"
 
@@ -54,13 +59,14 @@ main(argc, argv)
 int	argc;
 char	*argv[];
 {
-	extern double	l_redin(), l_grnin(), l_bluin(), atof();
+	extern double	l_redin(), l_grnin(), l_bluin(), l_brtin(), atof();
 	double	f;
 	int	a;
 	
 	funset(vcolin[RED], 1, l_redin);
 	funset(vcolin[GRN], 1, l_grnin);
 	funset(vcolin[BLU], 1, l_bluin);
+	funset(vbrtin, 1, l_brtin);
 	
 	for (a = 1; a < argc; a++)
 		if (argv[a][0] == '-')
@@ -160,8 +166,9 @@ eputs(" [-w][-x xr][-y yr][-e expr][-f file] [ [-s f][-c r g b] picture ..]\n");
 
 combine()			/* combine pictures */
 {
-	EPNODE	*coldef[3];
+	EPNODE	*coldef[3], *brtdef;
 	COLOR	*scanout;
+	double	d;
 	register int	i, j;
 						/* check defined variables */
 	for (j = 0; j < 3; j++) {
@@ -170,39 +177,53 @@ combine()			/* combine pictures */
 		else
 			coldef[j] = NULL;
 	}
+	if (vardefined(vbrtout))
+		brtdef = eparse(vbrtout);
+	else
+		brtdef = NULL;
+						/* predefine variables */
+	varset(vnfiles, (double)nfiles);
+	varset(vxres, (double)xres);
+	varset(vyres, (double)yres);
 						/* allocate scanline */
 	scanout = (COLOR *)emalloc(xres*sizeof(COLOR));
 						/* combine files */
 	for (ypos = yres-1; ypos >= 0; ypos--) {
-		for (i = 0; i < nfiles; i++)
-			if (freadscan(input[i].scan, xres, input[i].fp) < 0) {
-				eputs(input[i].name);
-				eputs(": read error\n");
-				quit(1);
+	    for (i = 0; i < nfiles; i++)
+		    if (freadscan(input[i].scan, xres, input[i].fp) < 0) {
+			    eputs(input[i].name);
+			    eputs(": read error\n");
+			    quit(1);
+		    }
+	    varset(vypos, (double)ypos);
+	    for (xpos = 0; xpos < xres; xpos++) {
+		varset(vxpos, (double)xpos);
+		eclock++;
+		if (brtdef != NULL) {
+		    d = evalue(brtdef);
+		    if (d < 0.0)
+			d = 0.0;
+		    setcolor(scanout[xpos], d, d, d);
+		} else {
+		    for (j = 0; j < 3; j++) {
+			if (coldef[j] != NULL) {
+				colval(scanout[xpos],j) = evalue(coldef[j]);
+			} else {
+			    colval(scanout[xpos],j) = 0.0;
+			    for (i = 0; i < nfiles; i++)
+				colval(scanout[xpos],j) +=
+					colval(input[i].coef,j) *
+					colval(input[i].scan[xpos],j);
 			}
-		varset(vypos, (double)ypos);
-		for (xpos = 0; xpos < xres; xpos++) {
-			varset(vxpos, (double)xpos);
-			eclock++;
-			for (j = 0; j < 3; j++) {
-				if (coldef[j] != NULL) {
-					colval(scanout[xpos],j) =
-						evalue(coldef[j]);
-				} else {
-					colval(scanout[xpos],j) = 0.0;
-					for (i = 0; i < nfiles; i++)
-						colval(scanout[xpos],j) +=
-						colval(input[i].coef,j) *
-						colval(input[i].scan[xpos],j);
-				}
-				if (colval(scanout[xpos],j) < 0.0)
-					colval(scanout[xpos],j) = 0.0;
-			}
+			if (colval(scanout[xpos],j) < 0.0)
+			    colval(scanout[xpos],j) = 0.0;
+		    }
 		}
-		if (fwritescan(scanout, xres, stdout) < 0) {
-			perror("write error");
-			quit(1);
-		}
+	    }
+	    if (fwritescan(scanout, xres, stdout) < 0) {
+		    perror("write error");
+		    quit(1);
+	    }
 	}
 	efree(scanout);
 }
@@ -240,6 +261,22 @@ double
 l_bluin()			/* get blue color */
 {
 	return(colin((int)(argument(1)+.5), BLU));
+}
+
+
+double
+l_brtin()			/* get brightness value */
+{
+	register int	fn;
+
+	fn = argument(1)+.5;
+	if (fn == 0)
+		return((double)nfiles);
+	if (fn < 1 || fn > nfiles) {
+		errno = EDOM;
+		return(0.0);
+	}
+	return(bright(input[fn-1].scan[xpos]));
 }
 
 
