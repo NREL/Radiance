44c229538c6013b5788aad819f735588d56aaab9
greg <greg>
1991-04-19 10:33:25 +0000

    added wildcard matching for formats

--- src/common/header.c
+++ src/common/header.c
@@ -8,9 +8,19 @@ static char SCCSid[] = "$SunId$ LBL";
  *  header.c - routines for reading and writing information headers.
  *
  *	8/19/88
+ *
+ *  printargs(ac,av,fp)	print an argument list to fp, followed by '\n'
+ *  isformat(s)		returns true if s is of the form "FORMAT=*"
+ *  formatval(r,s)	copy the format value in s to r
+ *  fputformat(s,fp)	write "FORMAT=%s" to fp
+ *  getheader(fp,f,p)	read header from fp, calling f(s,p) on each line
+ *  checkheader(i,p,o)	check header format from i against p and copy to o
+ *
+ *  To copy header from input to output, use getheader(fin, fputs, fout)
  */
 
 #include  <stdio.h>
+#include  <ctype.h>
 
 #define  MAXLINE	512
 
@@ -43,8 +53,10 @@ register char  *r;
 register char  *s;
 {
 	s += FMTSTRL;
-	while (*s && *s != '\n')
-		*r++ = *s++;
+	while (isspace(*s)) s++;
+	if (!*s) { *r = '\0'; return; }
+	while(*s) *r++ = *s++;
+	while (isspace(r[-1])) r--;
 	*r = '\0';
 }
 
@@ -84,7 +96,7 @@ char  *p;
 
 struct check {
 	FILE	*fp;
-	char	fs[32];
+	char	fs[64];
 };
 
 
@@ -100,7 +112,68 @@ register struct check  *cp;
 }
 
 
-checkheader(fin, fmt, fout)	/* check data format in header */
+/*
+ * Copymatch(pat,str) checks pat for wildcards, and
+ * copies str into pat if there is a match (returning true).
+ */
+
+#ifdef COPYMATCH
+copymatch(pat, str)
+char	*pat, *str;
+{
+	int	docopy = 0;
+	register char	*p = pat, *s = str;
+
+	do {
+		switch (*p) {
+		case '?':			/* match any character */
+			if (!*s++)
+				return(0);
+			docopy++;
+			break;
+		case '*':			/* match any string */
+			while (p[1] == '*') p++;
+			do
+				if ( (p[1] == '?' || p[1] == *s)
+						&& copymatch(p+1,s) ) {
+					strcpy(pat, str);
+					return(1);
+				}
+			while (*s++);
+			return(0);
+		case '\\':			/* literal next */
+			p++;
+		/* fall through */
+		default:			/* normal character */
+			if (*p != *s)
+				return(0);
+			s++;
+			break;
+		}
+	} while (*p++);
+	if (docopy)
+		strcpy(pat, str);
+	return(1);
+}
+#else
+#define copymatch(pat, s)	(!strcmp(pat, s))
+#endif
+
+
+/*
+ * Checkheader(fin,fmt,fout) returns a value of 1 if the input format
+ * matches the specification in fmt, 0 if no input format was found,
+ * and -1 if the input format does not match or there is an
+ * error reading the header.  If fmt is NULL, then -1 is returned
+ * if any input format is found (or there is an error), and 0 otherwise.
+ * If fmt contains any '*' or '?' characters, then checkheader
+ * does wildcard expansion and copies a matching result into fmt.
+ * Be sure that fmt is big enough to hold the match in such cases!
+ * The input header (minus any format lines) is copied to fout
+ * if fout is not NULL.
+ */
+
+checkheader(fin, fmt, fout)
 FILE  *fin;
 char  *fmt;
 FILE  *fout;
@@ -111,7 +184,9 @@ FILE  *fout;
 	cdat.fs[0] = '\0';
 	if (getheader(fin, mycheck, &cdat) < 0)
 		return(-1);
-	if (fmt != NULL && cdat.fs[0] != '\0')
-		return(strcmp(fmt, cdat.fs) ? -1 : 1);
+	if (fmt == NULL && cdat.fs[0] != '\0')
+		return(-1);
+	if (cdat.fs[0] != '\0')
+		return(copymatch(fmt, cdat.fs) ? 1 : -1);
 	return(0);
 }
