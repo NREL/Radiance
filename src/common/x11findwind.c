03c3a9d1a58116eb3bb930673ccd73cd8d1f1c11
greg <greg>
1991-04-22 13:40:49 +0000

    Initial revision

---
+++ src/common/x11findwind.c
@@ -0,0 +1,42 @@
+/* Copyright (c) 1991 Regents of the University of California */
+
+#ifndef lint
+static char SCCSid[] = "$SunId$ LBL";
+#endif
+
+/*
+ * find a window by its name under X
+ *
+ *	4/22/91		Greg Ward
+ */
+
+#include <stdio.h>
+#include <X11/Xlib.h>
+
+
+Window
+xfindwind(dpy, win, name, depth)
+Display	*dpy;
+Window	win;
+char	*name;
+int	depth;
+{
+	char	*nr;
+	Window	rr, pr, *cl;
+	int	nc;
+
+	if (XFetchName(dpy, win, &nr)) {
+		register int	succ = !strcmp(nr, name);
+		free(nr);
+		if (succ) return(win);
+	}
+	if (depth == 0)		/* negative depths search all */
+		return(None);
+	if (!XQueryTree(dpy, win, &rr, &pr, &cl, &nc) || nc == 0)
+		return(None);
+	while (nc--)
+		if ((rr = xfindwind(dpy, cl[nc], name, depth-1)) != None)
+			break;
+	XFree((char *)cl);
+	return(rr);
+}
