ab20c44c2185450b52051fe78e99c4360a635e5b
Gregory J. Ward <gregoryjward@gmail.com>
1991-07-15 11:25:02 +0000

    Initial revision

---
+++ src/common/quit.c
@@ -0,0 +1,11 @@
+/* Copyright (c) 1991 Regents of the University of California */
+
+#ifndef lint
+static char SCCSid[] = "$SunId$ LBL";
+#endif
+
+quit(code)			/* quit program */
+int  code;
+{
+	exit(code);
+}
