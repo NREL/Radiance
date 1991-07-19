0f8d355f4260fdec4f3f14dfe88bb2d43542e6c7
greg <greg>
1991-07-19 09:43:40 +0000

    moved readfargs() and freefargs() to library

--- src/gen/xform.c
+++ src/gen/xform.c
@@ -16,6 +16,8 @@ static char SCCSid[] = "$SunId$ LBL";
 
 #include  <ctype.h>
 
+#include  "object.h"
+
 #include  "otypes.h"
 
 int  xac;				/* global xform argument count */
@@ -37,19 +39,7 @@ char  *idprefix = NULL;			/* prefix for object identifiers */
 
 FUN  ofun[NUMTYPES] = INIT_OTYPE;	/* default types and actions */
 
-typedef struct {
-	short  nsargs;			/* # of string arguments */
-	char  **sarg;			/* string arguments */
-	short  niargs;			/* # of integer arguments */
-	int  *iarg;			/* integer arguments */
-	short  nfargs;			/* # of float arguments */
-	double  *farg;			/* float arguments */
-}  FUNARGS;
-
-#define  MAXSTR		128		/* maximum string length */
-
-FUNARGS  *getfargs();
-char  *malloc();
+extern char  *malloc();
 
 #define  progname  (xav[0])
 
@@ -264,26 +254,28 @@ o_default(fin)			/* pass on arguments unchanged */
 FILE  *fin;
 {
 	register int  i;
-	register FUNARGS  *fa;
+	FUNARGS  fa;
 
-	if ((fa = getfargs(fin)) == NULL)
+	if (readfargs(&fa, fin) <= 0)
 		return(-1);
 					/* string arguments */
-	printf("%d", fa->nsargs);
-	for (i = 0; i < fa->nsargs; i++)
-		printf(" %s", fa->sarg[i]);
+	printf("%d", fa.nsargs);
+	for (i = 0; i < fa.nsargs; i++)
+		printf(" %s", fa.sarg[i]);
 	printf("\n");
+#ifdef  IARGS
 					/* integer arguments */
-	printf("%d", fa->niargs);
-	for (i = 0; i < fa->niargs; i++)
-		printf(" %d", fa->iarg[i]);
+	printf("%d", fa.niargs);
+	for (i = 0; i < fa.niargs; i++)
+		printf(" %d", fa.iarg[i]);
 	printf("\n");
+#endif
 					/* float arguments */
-	printf("%d", fa->nfargs);
-	for (i = 0; i < fa->nfargs; i++)
-		printf(" %18.12g", fa->farg[i]);
+	printf("%d", fa.nfargs);
+	for (i = 0; i < fa.nfargs; i++)
+		printf(" %18.12g", fa.farg[i]);
 	printf("\n");
-	freefargs(fa);
+	freefargs(&fa);
 	return(0);
 }
 
@@ -292,28 +284,30 @@ addxform(fin)			/* add xf arguments to strings */
 FILE  *fin;
 {
 	register int  i;
-	register FUNARGS  *fa;
+	FUNARGS  fa;
 
-	if ((fa = getfargs(fin)) == NULL)
+	if (readfargs(&fa, fin) <= 0)
 		return(-1);
 					/* string arguments */
-	printf("%d", fa->nsargs + xac-xfa);
-	for (i = 0; i < fa->nsargs; i++)
-		printf(" %s", fa->sarg[i]);
+	printf("%d", fa.nsargs + xac-xfa);
+	for (i = 0; i < fa.nsargs; i++)
+		printf(" %s", fa.sarg[i]);
 	for (i = xfa; i < xac; i++)	/* add xf arguments */
 		printf(" %s", xav[i]);
 	printf("\n");
+#ifdef  IARGS
 					/* integer arguments */
-	printf("%d", fa->niargs);
-	for (i = 0; i < fa->niargs; i++)
-		printf(" %d", fa->iarg[i]);
+	printf("%d", fa.niargs);
+	for (i = 0; i < fa.niargs; i++)
+		printf(" %d", fa.iarg[i]);
 	printf("\n");
+#endif
 					/* float arguments */
-	printf("%d", fa->nfargs);
-	for (i = 0; i < fa->nfargs; i++)
-		printf(" %18.12g", fa->farg[i]);
+	printf("%d", fa.nfargs);
+	for (i = 0; i < fa.nfargs; i++)
+		printf(" %18.12g", fa.farg[i]);
 	printf("\n");
-	freefargs(fa);
+	freefargs(&fa);
 	return(0);
 }
 
@@ -347,17 +341,17 @@ FILE  *fin;
 m_glow(fin)			/* transform arguments for proximity light */
 FILE  *fin;
 {
-	register FUNARGS  *fa;
+	FUNARGS  fa;
 
-	if ((fa = getfargs(fin)) == NULL)
+	if (readfargs(&fa, fin) <= 0)
 		return(-1);
-	if (fa->nsargs != 0 || fa->niargs != 0 || fa->nfargs != 4)
+	if (fa.nsargs != 0  || fa.nfargs != 4)
 		return(-1);
 	printf("0\n0\n4");
 	printf(" %18.12g %18.12g %18.12g",
-			fa->farg[0], fa->farg[1], fa->farg[2]);
-	printf(" %18.12g\n", fa->farg[3] * tot.sca);
-	freefargs(fa);
+			fa.farg[0], fa.farg[1], fa.farg[2]);
+	printf(" %18.12g\n", fa.farg[3] * tot.sca);
+	freefargs(&fa);
 	return(0);
 }
 
@@ -366,18 +360,18 @@ m_spot(fin)			/* transform arguments for spotlight */
 FILE  *fin;
 {
 	double  v[3];
-	register FUNARGS  *fa;
+	FUNARGS  fa;
 
-	if ((fa = getfargs(fin)) == NULL)
+	if (readfargs(&fa, fin) <= 0)
 		return(-1);
-	if (fa->nsargs != 0 || fa->niargs != 0 || fa->nfargs != 7)
+	if (fa.nsargs != 0  || fa.nfargs != 7)
 		return(-1);
 	printf("0\n0\n7");
 	printf(" %18.12g %18.12g %18.12g %18.12g\n",
-			fa->farg[0], fa->farg[1], fa->farg[2], fa->farg[3]);
-	multv3(v, fa->farg+4, tot.xfm);
+			fa.farg[0], fa.farg[1], fa.farg[2], fa.farg[3]);
+	multv3(v, fa.farg+4, tot.xfm);
 	printf("\t%18.12g %18.12g %18.12g\n", v[0], v[1], v[2]);
-	freefargs(fa);
+	freefargs(&fa);
 	return(0);
 }
 
@@ -386,19 +380,19 @@ m_dielectric(fin)		/* transform arguments for dielectric */
 FILE  *fin;
 {
 	double  pow();
-	register FUNARGS  *fa;
+	FUNARGS  fa;
 
-	if ((fa = getfargs(fin)) == NULL)
+	if (readfargs(&fa, fin) <= 0)
 		return(-1);
-	if (fa->nsargs != 0 || fa->niargs != 0 || fa->nfargs != 5)
+	if (fa.nsargs != 0  || fa.nfargs != 5)
 		return(-1);
 	printf("0\n0\n5");
 	printf(" %18.12g %18.12g %18.12g",
-		pow(fa->farg[0], 1.0/tot.sca),
-		pow(fa->farg[1], 1.0/tot.sca),
-		pow(fa->farg[2], 1.0/tot.sca));
-	printf(" %18.12g %18.12g\n", fa->farg[3], fa->farg[4]);
-	freefargs(fa);
+		pow(fa.farg[0], 1.0/tot.sca),
+		pow(fa.farg[1], 1.0/tot.sca),
+		pow(fa.farg[2], 1.0/tot.sca));
+	printf(" %18.12g %18.12g\n", fa.farg[3], fa.farg[4]);
+	freefargs(&fa);
 	return(0);
 }
 
@@ -407,24 +401,24 @@ m_interface(fin)		/* transform arguments for interface */
 FILE  *fin;
 {
 	double  pow();
-	register FUNARGS  *fa;
+	FUNARGS  fa;
 
-	if ((fa = getfargs(fin)) == NULL)
+	if (readfargs(&fa, fin) <= 0)
 		return(-1);
-	if (fa->nsargs != 0 || fa->niargs != 0 || fa->nfargs != 8)
+	if (fa.nsargs != 0  || fa.nfargs != 8)
 		return(-1);
 	printf("0\n0\n8\n");
 	printf("%18.12g %18.12g %18.12g",
-		pow(fa->farg[0], 1.0/tot.sca),
-		pow(fa->farg[1], 1.0/tot.sca),
-		pow(fa->farg[2], 1.0/tot.sca));
-	printf(" %18.12g\n", fa->farg[3]);
+		pow(fa.farg[0], 1.0/tot.sca),
+		pow(fa.farg[1], 1.0/tot.sca),
+		pow(fa.farg[2], 1.0/tot.sca));
+	printf(" %18.12g\n", fa.farg[3]);
 	printf("%18.12g %18.12g %18.12g",
-		pow(fa->farg[4], 1.0/tot.sca),
-		pow(fa->farg[5], 1.0/tot.sca),
-		pow(fa->farg[6], 1.0/tot.sca));
-	printf(" %18.12g\n", fa->farg[7]);
-	freefargs(fa);
+		pow(fa.farg[4], 1.0/tot.sca),
+		pow(fa.farg[5], 1.0/tot.sca),
+		pow(fa.farg[6], 1.0/tot.sca));
+	printf(" %18.12g\n", fa.farg[7]);
+	freefargs(&fa);
 	return(0);
 }
 
@@ -434,37 +428,36 @@ FILE  *fin;
 {
 	int  i;
 	double  v[3];
-	register FUNARGS  *fa;
+	FUNARGS  fa;
 
-	if ((fa = getfargs(fin)) == NULL)
+	if (readfargs(&fa, fin) <= 0)
 		return(-1);
-	if (fa->niargs != 0 || (fa->nfargs != 9 &&
-			fa->nfargs != 11 && fa->nfargs != 15))
+	if (fa.nfargs != 9 && fa.nfargs != 11 && fa.nfargs != 15)
 		return(-1);
 					/* string arguments */
-	printf("%d", fa->nsargs);
-	for (i = 0; i < fa->nsargs; i++)
-		printf(" %s", fa->sarg[i]);
-	printf("\n0\n%d\n", fa->nfargs);
+	printf("%d", fa.nsargs);
+	for (i = 0; i < fa.nsargs; i++)
+		printf(" %s", fa.sarg[i]);
+	printf("\n0\n%d\n", fa.nfargs);
 					/* anchor point */
-	multp3(v, fa->farg, tot.xfm);
+	multp3(v, fa.farg, tot.xfm);
 	printf(" %18.12g %18.12g %18.12g\n", v[0], v[1], v[2]);
 					/* right vector */
-	multv3(v, fa->farg+3, tot.xfm);
+	multv3(v, fa.farg+3, tot.xfm);
 	printf(" %18.12g %18.12g %18.12g\n", v[0], v[1], v[2]);
 					/* down vector */
-	multv3(v, fa->farg+6, tot.xfm);
+	multv3(v, fa.farg+6, tot.xfm);
 	printf(" %18.12g %18.12g %18.12g\n", v[0], v[1], v[2]);
 					/* forground and background */
-	if (fa->nfargs == 11)
-		printf(" %18.12g %18.12g\n", fa->farg[9], fa->farg[10]);
-	else if (fa->nfargs == 15) {
+	if (fa.nfargs == 11)
+		printf(" %18.12g %18.12g\n", fa.farg[9], fa.farg[10]);
+	else if (fa.nfargs == 15) {
 		printf(" %18.12g %18.12g %18.12g\n",
-				fa->farg[9], fa->farg[10], fa->farg[11]);
+				fa.farg[9], fa.farg[10], fa.farg[11]);
 		printf(" %18.12g %18.12g %18.12g\n",
-				fa->farg[12], fa->farg[13], fa->farg[14]);
+				fa.farg[12], fa.farg[13], fa.farg[14]);
 	}
-	freefargs(fa);
+	freefargs(&fa);
 	return(0);
 }
 
@@ -473,19 +466,19 @@ o_source(fin)			/* transform source arguments */
 FILE  *fin;
 {
 	double  dv[3];
-	register FUNARGS  *fa;
+	FUNARGS  fa;
 
-	if ((fa = getfargs(fin)) == NULL)
+	if (readfargs(&fa, fin) <= 0)
 		return(-1);
-	if (fa->nsargs != 0 || fa->niargs != 0 || fa->nfargs != 4)
+	if (fa.nsargs != 0  || fa.nfargs != 4)
 		return(-1);
 					/* transform direction vector */
-	multv3(dv, fa->farg, tot.xfm);
+	multv3(dv, fa.farg, tot.xfm);
 					/* output */
 	printf("0\n0\n4");
 	printf(" %18.12g %18.12g %18.12g %18.12g\n",
-			dv[0], dv[1], dv[2], fa->farg[3]);
-	freefargs(fa);
+			dv[0], dv[1], dv[2], fa.farg[3]);
+	freefargs(&fa);
 	return(0);
 }
 
@@ -494,21 +487,21 @@ o_sphere(fin)			/* transform sphere arguments */
 FILE  *fin;
 {
 	double  cent[3], rad;
-	register FUNARGS  *fa;
+	FUNARGS  fa;
 
-	if ((fa = getfargs(fin)) == NULL)
+	if (readfargs(&fa, fin) <= 0)
 		return(-1);
-	if (fa->nsargs != 0 || fa->niargs != 0 || fa->nfargs != 4)
+	if (fa.nsargs != 0  || fa.nfargs != 4)
 		return(-1);
 	
-	multp3(cent, fa->farg, tot.xfm);	/* transform center */
+	multp3(cent, fa.farg, tot.xfm);	/* transform center */
 	
-	rad = fa->farg[3] * tot.sca;		/* scale radius */
+	rad = fa.farg[3] * tot.sca;		/* scale radius */
 	
 	printf("0\n0\n4");
 	printf(" %18.12g %18.12g %18.12g %18.12g\n",
 			cent[0], cent[1], cent[2], rad);
-	freefargs(fa);
+	freefargs(&fa);
 	return(0);
 }
 
@@ -518,23 +511,23 @@ FILE  *fin;
 {
 	double  p[3];
 	register int  i;
-	register FUNARGS  *fa;
+	FUNARGS  fa;
 
-	if ((fa = getfargs(fin)) == NULL)
+	if (readfargs(&fa, fin) <= 0)
 		return(-1);
-	if (fa->nsargs != 0 || fa->niargs != 0 || fa->nfargs % 3)
+	if (fa.nsargs != 0  || fa.nfargs % 3)
 		return(-1);
 	
-	printf("0\n0\n%d\n", fa->nfargs);
+	printf("0\n0\n%d\n", fa.nfargs);
 	
-	for (i = 0; i < fa->nfargs; i += 3) {
+	for (i = 0; i < fa.nfargs; i += 3) {
 		if (reverse)
-			multp3(p, fa->farg+(fa->nfargs-i-3), tot.xfm);
+			multp3(p, fa.farg+(fa.nfargs-i-3), tot.xfm);
 		else
-			multp3(p, fa->farg+i, tot.xfm);
+			multp3(p, fa.farg+i, tot.xfm);
 		printf(" %18.12g %18.12g %18.12g\n", p[0], p[1], p[2]);
 	}
-	freefargs(fa);
+	freefargs(&fa);
 	return(0);
 }
 
@@ -543,24 +536,24 @@ o_cone(fin)			/* transform cone and cup arguments */
 FILE  *fin;
 {
 	double  p0[3], p1[3], r0, r1;
-	register FUNARGS  *fa;
+	FUNARGS  fa;
 
-	if ((fa = getfargs(fin)) == NULL)
+	if (readfargs(&fa, fin) <= 0)
 		return(-1);
-	if (fa->nsargs != 0 || fa->niargs != 0 || fa->nfargs != 8)
+	if (fa.nsargs != 0  || fa.nfargs != 8)
 		return(-1);
 
 	printf("0\n0\n8\n");
 
-	multp3(p0, fa->farg, tot.xfm);
-	multp3(p1, fa->farg+3, tot.xfm);
-	r0 = fa->farg[6] * tot.sca;
-	r1 = fa->farg[7] * tot.sca;
+	multp3(p0, fa.farg, tot.xfm);
+	multp3(p1, fa.farg+3, tot.xfm);
+	r0 = fa.farg[6] * tot.sca;
+	r1 = fa.farg[7] * tot.sca;
 	printf(" %18.12g %18.12g %18.12g\n", p0[0], p0[1], p0[2]);
 	printf(" %18.12g %18.12g %18.12g\n", p1[0], p1[1], p1[2]);
 	printf(" %18.12g %18.12g\n", r0, r1);
 
-	freefargs(fa);
+	freefargs(&fa);
 	return(0);
 }
 
@@ -569,22 +562,22 @@ o_cylinder(fin)			/* transform cylinder and tube arguments */
 FILE  *fin;
 {
 	double  p0[3], p1[3], rad;
-	register FUNARGS  *fa;
+	FUNARGS  fa;
 
-	if ((fa = getfargs(fin)) == NULL)
+	if (readfargs(&fa, fin) <= 0)
 		return(-1);
-	if (fa->nsargs != 0 || fa->niargs != 0 || fa->nfargs != 7)
+	if (fa.nsargs != 0  || fa.nfargs != 7)
 		return(-1);
 
 	printf("0\n0\n7\n");
 
-	multp3(p0, fa->farg, tot.xfm);
-	multp3(p1, fa->farg+3, tot.xfm);
-	rad = fa->farg[6] * tot.sca;
+	multp3(p0, fa.farg, tot.xfm);
+	multp3(p1, fa.farg+3, tot.xfm);
+	rad = fa.farg[6] * tot.sca;
 	printf(" %18.12g %18.12g %18.12g\n", p0[0], p0[1], p0[2]);
 	printf(" %18.12g %18.12g %18.12g\n", p1[0], p1[1], p1[2]);
 	printf(" %18.12g\n", rad);
-	freefargs(fa);
+	freefargs(&fa);
 	return(0);
 }
 
@@ -593,97 +586,24 @@ o_ring(fin)			/* transform ring arguments */
 FILE  *fin;
 {
 	double  p0[3], pd[3], r0, r1;
-	register FUNARGS  *fa;
+	FUNARGS  fa;
 
-	if ((fa = getfargs(fin)) == NULL)
+	if (readfargs(&fa, fin) <= 0)
 		return(-1);
-	if (fa->nsargs != 0 || fa->niargs != 0 || fa->nfargs != 8)
+	if (fa.nsargs != 0  || fa.nfargs != 8)
 		return(-1);
 
 	printf("0\n0\n8\n");
 
-	multp3(p0, fa->farg, tot.xfm);
-	multv3(pd, fa->farg+3, tot.xfm);
-	r0 = fa->farg[6] * tot.sca;
-	r1 = fa->farg[7] * tot.sca;
+	multp3(p0, fa.farg, tot.xfm);
+	multv3(pd, fa.farg+3, tot.xfm);
+	r0 = fa.farg[6] * tot.sca;
+	r1 = fa.farg[7] * tot.sca;
 	printf(" %18.12g %18.12g %18.12g\n", p0[0], p0[1], p0[2]);
 	printf(" %18.12g %18.12g %18.12g\n", pd[0], pd[1], pd[2]);
 	printf(" %18.12g %18.12g\n", r0, r1);
-	freefargs(fa);
+	freefargs(&fa);
 	return(0);
-}
-
-
-FUNARGS *
-getfargs(fp)		/* get function arguments from stream */
-FILE  *fp;
-{
-	char  *strcpy();
-	char  sbuf[MAXSTR];
-	int  n;
-	register FUNARGS  *fa;
-	register int  i;
-
-	if ((fa = (FUNARGS *)malloc((unsigned)sizeof(FUNARGS))) == NULL)
-		goto memerr;
-	if (fscanf(fp, "%d", &n) != 1 || n < 0)
-		return(NULL);
-	if (fa->nsargs = n) {
-		fa->sarg = (char **)malloc((unsigned)fa->nsargs*sizeof(char *));
-		if (fa->sarg == NULL)
-			goto memerr;
-		for (i = 0; i < fa->nsargs; i++) {
-			if (fscanf(fp, "%s", sbuf) != 1)
-				return(NULL);
-			if ((fa->sarg[i] = malloc((unsigned)strlen(sbuf)+1)) == NULL)
-				goto memerr;
-			strcpy(fa->sarg[i], sbuf);
-		}
-	} else
-		fa->sarg = NULL;
-	if (fscanf(fp, "%d", &n) != 1 || n < 0)
-		return(NULL);
-	if (fa->niargs = n) {
-		fa->iarg = (int *)malloc((unsigned)fa->niargs*sizeof(int));
-		if (fa->iarg == NULL)
-			goto memerr;
-		for (i = 0; i < fa->niargs; i++)
-			if (fscanf(fp, "%d", &fa->iarg[i]) != 1)
-				return(NULL);
-	} else
-		fa->iarg = NULL;
-	if (fscanf(fp, "%d", &n) != 1 || n < 0)
-		return(NULL);
-	if (fa->nfargs = n) {
-		fa->farg = (double *)malloc((unsigned)fa->nfargs*sizeof(double));
-		if (fa->farg == NULL)
-			goto memerr;
-		for (i = 0; i < fa->nfargs; i++)
-			if (fscanf(fp, "%lf", &fa->farg[i]) != 1)
-				return(NULL);
-	} else
-		fa->farg = NULL;
-	return(fa);
-memerr:
-	fprintf(stderr, "%s: out of memory in getfargs\n", progname);
-	exit(1);
-}
-
-
-freefargs(fa)			/* free memory associated with fa */
-register FUNARGS  *fa;
-{
-	register int  i;
-
-	for (i = 0; i < fa->nsargs; i++)
-		free(fa->sarg[i]);
-	if (fa->nsargs)
-		free(fa->sarg);
-	if (fa->niargs)
-		free(fa->iarg);
-	if (fa->nfargs)
-		free(fa->farg);
-	free(fa);
 }
 
 
