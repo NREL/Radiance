d154df4f19f5fc863ae2f8680e3b6166ca96f5ff
greg <greg>
1991-08-20 10:02:43 +0000

    made ambient value weighting incremental (fewer stored values)

--- src/rt/ambient.h
+++ src/rt/ambient.h
@@ -35,3 +35,5 @@ extern double  maxarad;		/* maximum ambient radius */
 extern double  minarad;		/* minimum ambient radius */
 
 extern double  sumambient(), doambient(), makeambient();
+
+#define  AVGREFL	0.5	/* assumed average reflectance */
