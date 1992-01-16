e2c4ee4ed5fdd3319ddc62b41d33c49c6c77f070
Gregory J. Ward <gregoryjward@gmail.com>
1992-01-16 12:05:20 +0000

    changed default values and handling of specthresh

--- src/rt/rtrace.c
+++ src/rt/rtrace.c
@@ -51,7 +51,7 @@ int  vspretest = 512;			/* virtual source pretest density */
 int  directinvis = 0;			/* sources invisible? */
 double  srcsizerat = .25;		/* maximum ratio source size/dist. */
 
-double  specthresh = .5;		/* specular sampling threshold */
+double  specthresh = .15;		/* specular sampling threshold */
 double  specjitter = 1.;		/* specular sampling jitter */
 
 int  maxdepth = 6;			/* maximum recursion depth */
