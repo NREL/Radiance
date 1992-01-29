434f2c81b2b5ba3c0c9817123e40e4e5ab618a28
Gregory J. Ward <gregoryjward@gmail.com>
1992-01-29 15:18:34 +0000

    removed line buffering of output

--- src/px/mt160r.c
+++ src/px/mt160r.c
@@ -65,6 +65,9 @@ char  *fname;
 	}
 
 	fputs("\033[6~\033[7z", stdout);
+#ifdef _IOLBF
+	stdout->_flag &= ~_IOLBF;
+#endif
 	
 	for (i = yres-1; i >= 0; i--) {
 		if (freadcolrs(scanline, xres, input) < 0) {
@@ -110,6 +113,7 @@ int  y;
 		}
 		putchar('\r');
 		putchar('\n');
+		fflush(stdout);
 	}
 }
 
