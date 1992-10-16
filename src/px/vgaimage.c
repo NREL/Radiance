d9d2145a3ef83842f7be100d07ce22aab98602c6
Gregory J. Ward <gregoryjward@gmail.com>
1992-10-16 13:09:23 +0000

    Changes for 32-bit PC port

--- src/px/vgaimage.c
+++ src/px/vgaimage.c
@@ -16,62 +16,62 @@ static char SCCSid[] = "$SunId$ LBL";
 #include <dos.h>
 #include <i86.h>
 
-#define  M_RDOWN        0x8
-#define  M_RUP          0x10
-#define  M_LDOWN        0x2
-#define  M_LUP          0x4
-#define  M_MOTION       0x1
+#define	 M_RDOWN	0x8
+#define	 M_RUP		0x10
+#define	 M_LDOWN	0x2
+#define	 M_LUP		0x4
+#define	 M_MOTION	0x1
 
 int crad;
 int mouse_event = 0;
 int mouse_xpos = -1;
 int mouse_ypos = -1;
 
-#define  hide_cursor	move_cursor(-1,-1)
-#define  show_cursor	move_cursor(mouse_xpos,mouse_ypos)
+#define	 hide_cursor()	move_cursor(-1,-1)
+#define	 show_cursor()	move_cursor(mouse_xpos,mouse_ypos)
 
-#define  CTRL(c)        ((c)-'@')
+#define	 CTRL(c)	((c)-'@')
 
-#define  MAXWIDTH       1024
-#define  MAXHEIGHT      768
+#define	 MAXWIDTH	1024
+#define	 MAXHEIGHT	768
 
 short  ourblack = 0; ourwhite = 1;
 
-double  gamcor = 2.2;                   /* gamma correction */
+double	gamcor = 2.2;			/* gamma correction */
 
-int  dither = 1;                        /* dither colors? */
+int  dither = 1;			/* dither colors? */
 
-int  maxcolors = 0;                     /* maximum colors */
-int  minpix = 0;                        /* minimum pixel value */
-int  greyscale = 0;                     /* in grey */
+int  maxcolors = 0;			/* maximum colors */
+int  minpix = 0;			/* minimum pixel value */
+int  greyscale = 0;			/* in grey */
 
-int  scale = 0;                         /* scalefactor; power of two */
+int  scale = 0;				/* scalefactor; power of two */
 
-COLR  scanline[MAXWIDTH];               /* scan line buffer */
+COLR  scanline[MAXWIDTH];		/* scan line buffer */
 
-int  xmax, ymax;                        /* picture dimensions */
-FILE  *fin = stdin;                     /* input file */
-long  scanpos[MAXHEIGHT];               /* scan line positions in file */
-int  cury = 0;                          /* current scan location */
+int  xmax, ymax;			/* picture dimensions */
+FILE  *fin = stdin;			/* input file */
+long  scanpos[MAXHEIGHT];		/* scan line positions in file */
+int  cury = 0;				/* current scan location */
 
-double  exposure = 1.0;                 /* exposure compensation used */
+double	exposure = 1.0;			/* exposure compensation used */
 
-int  wrongformat = 0;                   /* input in another format? */
+int  wrongformat = 0;			/* input in another format? */
 
 struct {
 	int  xmin, ymin, xsiz, ysiz;
-}  box = {0, 0, 0, 0};                  /* current box */
+}  box = {0, 0, 0, 0};			/* current box */
 
 int  initialized = 0;
 int  cheight, cwidth;
 
-#define postext(x,y)    _settextposition(1+(y)/cheight,1+(x)/cwidth)
+#define postext(x,y)	_settextposition(1+(y)/cheight,1+(x)/cwidth)
 
 char  *progname;
 
 char  errmsg[128];
 
-extern BYTE  clrtab[256][3];            /* global color map */
+extern BYTE  clrtab[256][3];		/* global color map */
 
 extern long  ftell();
 
@@ -84,7 +84,7 @@ char  *argv[];
 	char  *gv;
 	int  headline();
 	int  i;
-	
+
 	progname = argv[0] = fixargv0(argv[0]);
 	if ((gv = getenv("GAMMA")) != NULL)
 		gamcor = atof(gv);
@@ -118,7 +118,7 @@ char  *argv[];
 		else
 			break;
 
-	if (i == argc-1) {              /* open picture */
+	if (i == argc-1) {		/* open picture */
 		fin = fopen(argv[i], "r");
 		if (fin == NULL) {
 			sprintf(errmsg, "cannot open file \"%s\"", argv[i]);
@@ -135,9 +135,9 @@ char  *argv[];
 	if (xmax > MAXWIDTH | ymax > MAXHEIGHT)
 		quiterr("input picture too large for VGA");
 
-	init();                 /* initialize and display */
+	init();			/* initialize and display */
 
-	while (docommand())     /* loop on command */
+	while (docommand())	/* loop on command */
 		;
 	quiterr(NULL);
 userr:
@@ -147,7 +147,7 @@ userr:
 }
 
 
-headline(s)             /* get relevant info from header */
+headline(s)		/* get relevant info from header */
 char  *s;
 {
 	char  fmt[32];
@@ -161,20 +161,20 @@ char  *s;
 }
 
 
-init()                  /* initialize and load display */
+init()			/* initialize and load display */
 {
 	static struct {
-		short   mode;
-		short   xsiz, ysiz;
-	} video[] = {    
+		short	mode;
+		short	xsiz, ysiz;
+	} video[] = {
 		{_MRES256COLOR, 320, 200},
 		{_VRES256COLOR, 640, 400},
 		{_SVRES256COLOR, 800, 600},
 		{_XRES256COLOR, 1024, 768},
 		-1
 	};
-	struct videoconfig      config;
-	register int    i;
+	struct videoconfig	config;
+	register int	i;
 				/* pick a card... */
 	for (i = 0; video[i].mode != -1; i++)
 		if (video[i].xsiz >= xmax && video[i].ysiz >= ymax)
@@ -204,7 +204,7 @@ init()                  /* initialize and load display */
 }
 
 
-quiterr(err)            /* print message and exit */
+quiterr(err)		/* print message and exit */
 char  *err;
 {
 	if (initialized)
@@ -218,35 +218,35 @@ char  *err;
 
 
 int
-docommand()                     /* execute command */
+docommand()			/* execute command */
 {
 	char  buf[64];
 	COLOR  cval;
 	int  com;
-	double  comp;
+	double	comp;
 
 	while (!kbhit())
 		watch_mouse();
 	com = getch();
-	switch (com) {                  /* interpret command */
+	switch (com) {			/* interpret command */
 	case 'q':
-	case CTRL('Z'):                         /* quit */
+	case CTRL('Z'):				/* quit */
 		return(0);
 	case '\n':
 	case '\r':
 	case 'l':
-	case 'c':                               /* value */
+	case 'c':				/* value */
 		if (avgbox(cval) == -1)
 			return(-1);
 		switch (com) {
 		case '\n':
-		case '\r':                              /* radiance */
+		case '\r':				/* radiance */
 			sprintf(buf, "%.3f", intens(cval)/exposure);
 			break;
-		case 'l':                               /* luminance */
+		case 'l':				/* luminance */
 			sprintf(buf, "%.0fL", luminance(cval)/exposure);
 			break;
-		case 'c':                               /* color */
+		case 'c':				/* color */
 			comp = pow(2.0, (double)scale);
 			sprintf(buf, "(%.2f,%.2f,%.2f)",
 					colval(cval,RED)*comp,
@@ -265,10 +265,10 @@ docommand()                     /* execute command */
 }
 
 
-watch_mouse()                   /* look after mousie */
+watch_mouse()			/* look after mousie */
 {
-	static int      mouse_installed = 0;
-	int     a_x, a_y, l_x, l_y;
+	static int	mouse_installed = 0;
+	int	a_x, a_y, l_x, l_y;
 
 	if (!mouse_installed) {
 		ms_init();
@@ -280,7 +280,7 @@ watch_mouse()                   /* look after mousie */
 		return;
 	l_x = a_x = mouse_xpos; l_y = a_y = mouse_ypos;
 	hide_cursor();
-	revbox(a_x, a_y, l_x, l_y);             /* show box */
+	revbox(a_x, a_y, l_x, l_y);		/* show box */
 	do {
 		mouse_event = 0;
 		while (!mouse_event)
@@ -290,7 +290,7 @@ watch_mouse()                   /* look after mousie */
 			revbox(a_x, a_y, l_x=mouse_xpos, l_y=mouse_ypos);
 		}
 	} while (!(mouse_event & M_LUP));
-	revbox(a_x, a_y, l_x, l_y);             /* hide box */
+	revbox(a_x, a_y, l_x, l_y);		/* hide box */
 	show_cursor();
 	box.xmin = mouse_xpos;
 	box.ymin = mouse_ypos;
@@ -310,7 +310,7 @@ watch_mouse()                   /* look after mousie */
 }
 
 
-revbox(x0, y0, x1, y1)                  /* draw box with reversed lines */
+revbox(x0, y0, x1, y1)			/* draw box with reversed lines */
 int  x0, y0, x1, y1;
 {
 	_setplotaction(_GXOR);
@@ -324,14 +324,14 @@ int  x0, y0, x1, y1;
 
 
 int
-avgbox(clr)                             /* average color over current box */
+avgbox(clr)				/* average color over current box */
 COLOR  clr;
 {
 	static COLOR  lc;
 	static int  ll, lr, lt, lb;
 	int  left, right, top, bottom;
 	int  y;
-	double  d;
+	double	d;
 	COLOR  ctmp;
 	register int  x;
 
@@ -372,10 +372,10 @@ COLOR  clr;
 }
 
 
-setpalette()                    /* set our palette using clrtab */
+setpalette()			/* set our palette using clrtab */
 {
-	long    cvals[256];
-	register int    i;
+	long	cvals[256];
+	register int	i;
 
 	if (minpix >= 2) {
 		cvals[ourblack] = _BLACK; cvals[ourwhite] = _BRIGHTWHITE;
@@ -388,11 +388,11 @@ setpalette()                    /* set our palette using clrtab */
 }
 
 
-greyimage()                     /* display greyscale image */
+greyimage()			/* display greyscale image */
 {
-	short   thiscolor, lastcolor = -1;
-	int     y;
-	register int    x;
+	short	thiscolor, lastcolor = -1;
+	int	y;
+	register int	x;
 					/* set gamma correction */
 	setcolrgam(gamcor);
 					/* set up color map */
@@ -408,7 +408,7 @@ greyimage()                     /* display greyscale image */
 			shiftcolrs(scanline, xmax, scale);
 		colrs_gambs(scanline, xmax);
 		if (maxcolors < 256)
-			for (x = 0; x < xmax; x++) { 
+			for (x = 0; x < xmax; x++) {
 				thiscolor = ((long)normbright(scanline[x]) *
 					maxcolors + 128) >> 8;
 				if (thiscolor != lastcolor)
@@ -426,11 +426,11 @@ greyimage()                     /* display greyscale image */
 }
 
 
-mappedimage()                   /* display color-mapped image */
+mappedimage()			/* display color-mapped image */
 {
-	BYTE    bscan[MAXWIDTH];
-	int     y;
-	register int    x;
+	BYTE	bscan[MAXWIDTH];
+	int	y;
+	register int	x;
 					/* set gamma correction */
 	setcolrgam(gamcor);
 					/* make histogram */
@@ -499,36 +499,34 @@ void _loadds far mouse_handler (int max, int mcx, int mdx)
 #pragma aux mouse_handler parm [EAX] [ECX] [EDX]
 	mouse_event = max;
 	mouse_xpos = mcx;
-	mouse_ypos = mdx * (long)ymax / 200;       /* kludge */
-	if (mouse_xpos >= xmax) mouse_xpos = xmax-1;
-	if (mouse_ypos >= ymax) mouse_ypos = ymax-1;
+	mouse_ypos = mdx;
 }
 #pragma on (check_stack)
 
 
 void
-move_cursor(newx, newy)         /* move cursor to new position */
+move_cursor(newx, newy)		/* move cursor to new position */
 int  newx, newy;
 {
 	static char  *imp = NULL;
 	static int  curx = -1, cury = -1;
-#define xcmin           (curx-crad<0 ? 0 : curx-crad)
-#define ycmin           (cury-crad<0 ? 0 : cury-crad)
-#define xcmax           (curx+crad>=xmax ? xmax-1 : curx+crad)
-#define ycmax           (cury+crad>=ymax ? ymax-1 : cury+crad)
+#define xcmin		(curx-crad<0 ? 0 : curx-crad)
+#define ycmin		(cury-crad<0 ? 0 : cury-crad)
+#define xcmax		(curx+crad>=xmax ? xmax-1 : curx+crad)
+#define ycmax		(cury+crad>=ymax ? ymax-1 : cury+crad)
 
 	if (newx == curx & newy == cury)
 		return;
-	if (imp == NULL && 
+	if (imp == NULL &&
 		(imp = bmalloc(_imagesize(0,0,2*crad+1,2*crad+1))) == NULL) {
 		quiterr("out of memory in move_cursor");
 	}
-	if (curx >= 0 & cury >= 0)      /* clear old cursor */
+	if (curx >= 0 & cury >= 0)	/* clear old cursor */
 		_putimage(xcmin, ycmin, imp, _GPSET);
 					/* record new position */
 	curx = newx; cury = newy;
 	if (curx < 0 | cury < 0)
-		return;         /* no cursor */
+		return;		/* no cursor */
 					/* save under new cursor */
 	_getimage(xcmin, ycmin, xcmax, ycmax, imp);
 					/* draw new cursor */
@@ -567,15 +565,26 @@ ms_init()
     }
 
     crad = ymax/40;
-    
+
+    /* set screen limits */
+
+    inregs.w.ax = 0x7;		/* horizontal resolution */
+    inregs.w.cx = 0;
+    inregs.w.dx = xmax-1;
+    int386x( 0x33, &inregs, &outregs, &sregs );
+    inregs.w.ax = 0x8;		/* vertical resolution */
+    inregs.w.cx = 0;
+    inregs.w.dx = ymax-1;
+    int386x( 0x33, &inregs, &outregs, &sregs );
+
     /* install watcher */
 
     inregs.w.ax = 0xC;
     inregs.w.cx = M_LDOWN | M_LUP | M_MOTION;
     function_ptr = mouse_handler;
     inregs.x.edx = FP_OFF( function_ptr );
-    sregs.es     = FP_SEG( function_ptr );
+    sregs.es	 = FP_SEG( function_ptr );
     int386x( 0x33, &inregs, &outregs, &sregs );
-    
+
     return(1);
 }
