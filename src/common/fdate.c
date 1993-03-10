e5aeac0a2811d9255d5b66572f644b49b5e83dd6
greg <greg>
1993-03-10 19:39:54 +0000

    Initial revision

---
+++ src/common/fdate.c
@@ -0,0 +1,25 @@
+/* Copyright (c) 1993 Regents of the University of California */
+
+#ifndef lint
+static char SCCSid[] = "$SunId$ LBL";
+#endif
+
+/*
+ * Return file date (UNIX seconds as returned by time(2) call)
+ */
+
+#include  <sys/types.h>
+#include  <sys/stat.h>
+
+
+long
+fdate(fname)				/* get file date */
+char  *fname;
+{
+	struct stat  sbuf;
+
+	if (stat(fname, &sbuf) == -1)
+		return(-1);
+
+	return(sbuf.st_mtime);
+}
