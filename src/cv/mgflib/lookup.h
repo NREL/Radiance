2a6fed1be0404ccfae3b4b77f2f85c9c7a838fdb
Gregory J. Ward <gregoryjward@gmail.com>
1994-06-25 16:34:50 +0000

    enhancements for general (non-string) associations

--- src/cv/mgflib/lookup.h
+++ src/cv/mgflib/lookup.h
@@ -3,23 +3,27 @@
 /* SCCSid "$SunId$ LBL" */
 
 /*
- * Header file for table lookup routines
+ * Header file for general associative table lookup routines
  */
 
 typedef struct {
-	char	*key;		/* key name (dynamically allocated) */
+	char	*key;		/* key name */
+	long	hval;		/* key hash value (for efficiency) */
 	char	*data;		/* pointer to client data */
 } LUENT;
 
 typedef struct {
-	void	(*freek)();	/* free entry key */
-	void	(*freed)();	/* free entry data */
+	long	(*hashf)();	/* key hash function */
+	int	(*keycmp)();	/* key comparison function */
+	void	(*freek)();	/* free a key */
+	void	(*freed)();	/* free the data */
 	int	tsiz;		/* current table size */
 	LUENT	*tabl;		/* table, if allocated */
 	int	ndel;		/* number of deleted entries */
 } LUTAB;
 
-#define LU_SINIT(fk,fd)		{(void (*)())(fk),(void (*)())(fd),0,NULL,0}
+#define LU_SINIT(fk,fd)		{lu_shash,strcmp,(void (*)())(fk),\
+				(void (*)())(fd),0,NULL,0}
 
 /*
  * The lu_init routine is called to initialize a table.  The number of
@@ -29,19 +33,25 @@ typedef struct {
  * and the expected (minimum) table size.  The value returned is the
  * actual allocated table size (or zero if there was insufficient memory).
  *
- * It isn't fully necessary to initialize the LUTAB structure.  If it
- * is cleared (tsiz = 0, tabl = NULL), then the first call to lu_find
- * will allocate a minimal table.  If key and data free functions are
- * to be used, the LU_SINIT macro provides a convenient declaration.
+ * The hashf, keycmp, freek and freed member functions must be assigned
+ * separately.  If the hash value is sufficient to guarantee equality
+ * between keys, then the keycmp pointer may be NULL.  Otherwise, it
+ * should return 0 if the two passed keys match.  If it is not necessary
+ * (or possible) to free the key and/or data values, then the freek and/or
+ * freed member functions may be NULL.
+ *
+ * It isn't fully necessary to call lu_init to initialize the LUTAB structure.
+ * If tsiz is 0, then the first call to lu_find will allocate a minimal table.
+ * The LU_SINIT macro provides a convenient static declaration for character
+ * string keys.
  *
  * The lu_find routine returns the entry corresponding to the given
- * key (any nul-terminated string).  If the entry does not exist, the
- * corresponding key field will be NULL.  If the entry has been
- * previously deleted but not yet freed, then only the data field
- * will be NULL.  It is the caller's responsibility to (allocate and)
- * assign the key and data fields when creating a new entry.
- * The only case where lu_find returns NULL is when the system
- * has run out of memory.
+ * key.  If the entry does not exist, the corresponding key field will
+ * be NULL.  If the entry has been previously deleted but not yet freed,
+ * then only the data field will be NULL.  It is the caller's
+ * responsibility to (allocate and) assign the key and data fields when
+ * creating a new entry.  The only case where lu_find returns NULL is when
+ * the system has run out of memory.
  *
  * The lu_delete routine frees an entry's data (if any) by calling
  * the freed member function, but does not free the key field.  This
@@ -59,11 +69,13 @@ extern int	lu_init();
 extern LUENT	*lu_find();
 extern void	lu_delete();
 extern void	lu_done();
-extern int	lu_hash();
+extern long	lu_shash();
 #else
 extern int	lu_init(LUTAB *, int);
 extern LUENT	*lu_find(LUTAB *, char *);
 extern void	lu_delete(LUTAB *, char *);
 extern void	lu_done(LUTAB *);
-extern int	lu_hash(char *);
+extern long	lu_shash(char *);
 #endif
+
+extern int	strcmp();
