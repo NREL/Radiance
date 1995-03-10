6c33964fa939ea7141177a05a475e17cdd207aba
greg <greg>
1995-03-10 15:16:42 +0000

    moved CIE definitions to context.c

--- src/cv/mgflib/parser.h
+++ src/cv/mgflib/parser.h
@@ -206,9 +206,9 @@ extern double	normalize(FVECT);	/* normalize a vector */
 
 typedef struct {
 	int	clock;			/* incremented each change */
-	unsigned short	flags;		/* what's been set */
-	unsigned short	ssamp[C_CNSS];	/* spectral samples, min wl to max */
-	unsigned long	ssum;		/* straight sum of spectral values */
+	short	flags;			/* what's been set */
+	short	ssamp[C_CNSS];		/* spectral samples, min wl to max */
+	long	ssum;			/* straight sum of spectral values */
 	float	cx, cy;			/* xy chromaticity value */
 	float	eff;			/* efficacy (lumens/watt) */
 } C_COLOR;
@@ -222,24 +222,6 @@ typedef struct {
 			C_CMAXV,C_CMAXV,C_CMAXV,C_CMAXV,C_CMAXV,C_CMAXV,\
 			C_CMAXV,C_CMAXV,C_CMAXV,C_CMAXV,C_CMAXV,C_CMAXV},\
 			(long)C_CNSS*C_CMAXV, 1./3., 1./3., 178.006 }
-
-#define C_CIEX		{ 1, C_CDSPEC|C_CSSPEC|C_CSXY|C_CSEFF,\
-			{14,42,143,435,1344,2839,3483,3362,2908,1954,956,\
-			320,49,93,633,1655,2904,4334,5945,7621,9163,10263,\
-			10622,10026,8544,6424,4479,2835,1649,874,468,227,\
-			114,58,29,14,7,3,2,1,0}, 106836L, .467, .368, 362.230 }
-
-#define C_CIEY		{ 1, C_CDSPEC|C_CSSPEC|C_CSXY|C_CSEFF,\
-			{0,1,4,12,40,116,230,380,600,910,1390,2080,3230,\
-			5030,7100,8620,9540,9950,9950,9520,8700,7570,6310,\
-			5030,3810,2650,1750,1070,610,320,170,82,41,21,10,\
-			5,2,1,1,0,0}, 106856L, .398, .542, 493.525 }
-
-#define C_CIEZ		{ 1, C_CDSPEC|C_CSSPEC|C_CSXY|C_CSEFF,\
-			{65,201,679,2074,6456,13856,17471,17721,16692,\
-			12876,8130,4652,2720,1582,782,422,203,87,39,21,17,\
-			11,8,3,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},\
-			106770L, .147, .077, 54.363 }
 
 #define c_cval(c,l)	((double)(c)->ssamp[((l)-C_MINWL)/C_CWLI] / (c)->ssum)
 
