d86e1ff309c3bafcff30fa3d405e18fa14626238
Gregory J. Ward <gregoryjward@gmail.com>
1995-10-11 10:39:26 +0000

    Initial revision

---
+++ src/px/mx3.h
@@ -0,0 +1,34 @@
+/* Copyright (c) 1995 Regents of the University of California */
+
+/* SCCSid "$SunId$ LBL" */
+
+#ifndef MX3_HDR
+#define MX3_HDR
+
+#define mx3_copy(a, b) bcopy(a, b, 3*3*sizeof(double))
+
+/* FLOAT VERSIONS */
+float  mx3f_adjoint(/* a, b */);
+void  mx3f_mul(/* a, b, c */);
+void  mx3f_print(/* str, a */);
+void  mx3f_translate_mat(/* tx, ty, a */);
+void  mx3f_translate(/* a, tx, ty */);
+void  mx3f_scale_mat(/* sx, sy, a */);
+void  mx3f_scale(/* a, sx, sy */);
+void  mx3f_transform(/* p, a, q */);
+float  mx3f_transform_div(/* p, a, q */);
+#define mx3f_copy(a, b) bcopy(a, b, 3*3*sizeof(float))
+
+/* DOUBLE VERSIONS */
+double  mx3d_adjoint(/* a, b */);
+void  mx3d_mul(/* a, b, c */);
+void  mx3d_print(/* str, a */);
+void  mx3d_translate_mat(/* tx, ty, a */);
+void  mx3d_translate(/* a, tx, ty */);
+void  mx3d_scale_mat(/* sx, sy, a */);
+void  mx3d_scale(/* a, sx, sy */);
+void  mx3d_transform(/* p, a, q */);
+double  mx3d_transform_div(/* p, a, q */);
+#define mx3d_copy(a, b) bcopy(a, b, 3*3*sizeof(double))
+
+#endif
