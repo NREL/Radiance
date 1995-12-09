635e22fabb72c2fd3621a8aa12d7ebc4b85caaaa
greg <greg>
1995-12-09 09:40:42 +0000

    bug fixes, especially for rays that never entered mist volume

--- src/rt/m_mist.c
+++ src/rt/m_mist.c
@@ -43,7 +43,6 @@ static char SCCSid[] = "$SunId$ LBL";
  *  scattering.  A value approaching 1 indicates strong forward scattering.
  */
 
-#define MAXSLIST	32		/* maximum sources to check */
 #define RELAYDELIM	'>'		/* relay delimiter character */
 
 extern COLOR  cextinction;		/* global coefficient of extinction */
@@ -66,31 +65,45 @@ register int  n;
 
 
 static int
-srcmatch(sn, id)	/* check for an id match on a light source */
-register int  sn;
-char  *id;
+srcmatch(sp, id)	/* check for an id match on a light source */
+register SRCREC  *sp;
+register char  *id;
 {
 	extern char  *index();
 	register char  *cp;
-again:
-	if (source[sn].so == NULL)		/* just in case */
-		return(0);
-	if ((cp = index(id, RELAYDELIM)) != NULL) {	/* relay source */
-		if (!(source[sn].sflags & SVIRTUAL))
+						/* check for relay sources */
+	while ((cp = index(id, RELAYDELIM)) != NULL) {
+		if (!(sp->sflags & SVIRTUAL) || sp->so == NULL)
 			return(0);
-		*cp = '\0';
-		if (strcmp(id, source[sn].so->oname)) {
-			*cp = RELAYDELIM;
+		if (strncmp(id, sp->so->oname, cp-id) || sp->so->oname[cp-id])
 			return(0);
-		}
-		*cp = RELAYDELIM;
-		id = cp + 1;				/* recurse */
-		sn = source[sn].sa.sv.sn;
-		goto again;
+		id = cp + 1;				/* relay to next */
+		sp = source + sp->sa.sv.sn;
 	}
-	if (source[sn].sflags & SVIRTUAL)
+	if (sp->sflags & SVIRTUAL || sp->so == NULL)
 		return(0);
-	return(!strcmp(id, source[sn].so->oname));
+	return(!strcmp(id, sp->so->oname));
+}
+
+
+static
+add2slist(r, sl)	/* add source list to ray's */
+register RAY  *r;
+register int  *sl;
+{
+	static int  slspare[MAXSLIST+1];	/* in case of emergence */
+	register int  i;
+
+	if (sl == NULL || sl[0] == 0)		/* nothing to add */
+		return;
+	if (r->slights == NULL)
+		(r->slights = slspare)[0] = 0;	/* just once per ray path */
+	for (i = sl[0]; i > 0; i--)
+		if (!inslist(r->slights, sl[i])) {
+			if (r->slights[0] >= MAXSLIST)
+				error(USER, "scattering source list overflow");
+			r->slights[++r->slights[0]] = sl[i];
+		}
 }
 
 
@@ -107,25 +120,29 @@ register RAY  *r;
 					/* check arguments */
 	if (m->oargs.nfargs > 5)
 		objerror(m, USER, "bad arguments");
-	if (m->oargs.nsargs > MAXSLIST)
-		objerror(m, USER, "too many sources in list");
 					/* get source indices */
 	if (m->oargs.nsargs > 0 && (myslist = (int *)m->os) == NULL) {
+		if (m->oargs.nsargs > MAXSLIST)
+			objerror(m, USER, "too many sources in list");
 		myslist = (int *)malloc((m->oargs.nsargs+1)*sizeof(int));
 		if (myslist == NULL)
 			goto memerr;
-		myslist[0] = m->oargs.nsargs;	/* size is first in set */
-		for (j = myslist[0]; j > 0; j--) {
+		myslist[0] = 0;			/* size is first in list */
+		for (j = 0; j < m->oargs.nsargs; j++) {
 			i = nsources;		/* look up each source id */
 			while (i--)
-				if (srcmatch(i, m->oargs.sarg[j-1]))
+				if (srcmatch(source+i, m->oargs.sarg[j]))
 					break;
 			if (i < 0) {
 				sprintf(errmsg, "unknown source \"%s\"",
-						m->oargs.sarg[j-1]);
+						m->oargs.sarg[j]);
+				objerror(m, WARNING, errmsg);
+			} else if (inslist(myslist, i)) {
+				sprintf(errmsg, "duplicate source \"%s\"",
+						m->oargs.sarg[j]);
 				objerror(m, WARNING, errmsg);
 			} else
-				myslist[j] = i;
+				myslist[++myslist[0]] = i;
 		}
 		m->os = (char *)myslist;
 	}
@@ -141,7 +158,7 @@ register RAY  *r;
 		return(1);
 	VCOPY(p.rdir, r->rdir);
 	p.slights = newslist;
-	if (r->slights != NULL)
+	if (r->slights != NULL)			/* copy old list if one */
 		for (j = r->slights[0]; j >= 0; j--)
 			p.slights[j] = r->slights[j];
 	else
@@ -152,15 +169,25 @@ register RAY  *r;
 			p.albedo = m->oargs.farg[3];
 		if (m->oargs.nfargs > 4)
 			p.gecc = m->oargs.farg[4];
-		if (myslist != NULL)			/* add to list */
-			for (j = myslist[0]; j > 0; j--)
-				if (!inslist(p.slights, myslist[j])) {
-					if (p.slights[0] >= MAXSLIST)
-						error(USER,
-					"scattering source list overflow");
-					p.slights[++p.slights[0]] = myslist[j];
-				}
+		add2slist(&p, myslist);			/* add to list */
 	} else {				/* leaving ray */
+		if (myslist != NULL) {			/* delete from list */
+			for (j = myslist[0]; j > 0; j--)
+				if (i = inslist(p.slights, myslist[j]))
+					p.slights[i] = -1;
+			for (i = 0, j = 1; j <= p.slights[0]; j++)
+				if (p.slights[j] != -1)
+					p.slights[++i] = p.slights[j];
+			if (p.slights[0] - i < myslist[0]) {	/* fix old */
+				addcolor(r->cext, mext);
+				if (m->oargs.nfargs > 3)
+					r->albedo = m->oargs.farg[3];
+				if (m->oargs.nfargs > 4)
+					r->gecc = m->oargs.farg[4];
+				add2slist(r, myslist);
+			}
+			p.slights[0] = i;
+		}
 		re = colval(r->cext,RED) - colval(mext,RED);
 		ge = colval(r->cext,GRN) - colval(mext,GRN);
 		be = colval(r->cext,BLU) - colval(mext,BLU);
@@ -171,15 +198,6 @@ register RAY  *r;
 			p.albedo = salbedo;
 		if (m->oargs.nfargs > 4)
 			p.gecc = seccg;
-		if (myslist != NULL) {			/* delete from list */
-			for (j = myslist[0]; j > 0; j--)
-				if (i = inslist(p.slights, myslist[j]))
-					p.slights[i] = -1;
-			for (i = 0, j = 1; j <= p.slights[0]; j++)
-				if (p.slights[j] != -1)
-					p.slights[++i] = p.slights[j];
-			p.slights[0] = i;
-		}
 	}
 	rayvalue(&p);				/* calls rayparticipate() */
 	copycolor(r->rcol, p.rcol);		/* return value */
