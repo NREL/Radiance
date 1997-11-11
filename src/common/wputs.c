a3c6ae8b5b02c61f56a48c85c0c3d2b45a5d5ab4
Gregory J. Ward <gregoryjward@gmail.com>
1997-11-11 19:53:17 +0000

    Initial revision

---
+++ src/common/wputs.c
@@ -0,0 +1,15 @@
+/* Copyright (c) 1997 Silicon Graphics, Inc. */
+
+#ifndef lint
+static char SCCSid[] = "$SunId$ SGI";
+#endif
+
+/*
+ * Default warning output function.
+ */
+
+wputs(s)
+char	*s;
+{
+	eputs(s);
+}
