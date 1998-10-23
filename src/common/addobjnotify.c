e251aa38c692054190b1ff61c1b66bb78422c932
Gregory J. Ward <gregoryjward@gmail.com>
1998-10-23 09:26:29 +0000

    Initial revision

---
+++ src/common/addobjnotify.c
@@ -0,0 +1,11 @@
+/* Copyright (c) 1998 Silicon Graphics, Inc. */
+
+#ifndef lint
+static char SCCSid[] = "$SunId$ SGI";
+#endif
+
+/*
+ * Dummy declaration of addobjnotify[]
+ */
+
+int  (*addobjnotify[1])() = {0};
