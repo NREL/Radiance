9a614490d6f9ab1921b4f2ab77c3621e58ba38cf
greg <greg>
2003-02-21 22:27:52 +0000

    Updated for 3.5 release

--- src/common/xraster.c
+++ src/common/xraster.c
@@ -1,13 +1,62 @@
-/* Copyright 1988 Regents of the University of California */
-
-#ifndef lint
-static char SCCSid[] = "$SunId$ LBL";
-#endif
-
 /*
  * xraster.c - routines to handle images for X windows.
+ */
+
+/* ====================================================================
+ * The Radiance Software License, Version 1.0
  *
- *	2/18/88
+ * Copyright (c) 1990 - 2002 The Regents of the University of California,
+ * through Lawrence Berkeley National Laboratory.   All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *         notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *           if any, must include the following acknowledgment:
+ *             "This product includes Radiance software
+ *                 (http://radsite.lbl.gov/)
+ *                 developed by the Lawrence Berkeley National Laboratory
+ *               (http://www.lbl.gov/)."
+ *       Alternately, this acknowledgment may appear in the software itself,
+ *       if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Radiance," "Lawrence Berkeley National Laboratory"
+ *       and "The Regents of the University of California" must
+ *       not be used to endorse or promote products derived from this
+ *       software without prior written permission. For written
+ *       permission, please contact radiance@radsite.lbl.gov.
+ *
+ * 5. Products derived from this software may not be called "Radiance",
+ *       nor may "Radiance" appear in their name, without prior written
+ *       permission of Lawrence Berkeley National Laboratory.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.   IN NO EVENT SHALL Lawrence Berkeley National Laboratory OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of Lawrence Berkeley National Laboratory.   For more
+ * information on Lawrence Berkeley National Laboratory, please see
+ * <http://www.lbl.gov/>.
  */
 
 #include <stdio.h>
@@ -33,7 +82,7 @@ register XRASTER	*xr;
 	if (xr->pixels == NULL)
 		return(NULL);
 	if (XGetColorCells(0, xr->ncolors, 0, &j, xr->pixels) == 0) {
-		free((char *)xr->pixels);
+		free((void *)xr->pixels);
 		xr->pixels = NULL;
 		return(NULL);
 	}
@@ -86,7 +135,7 @@ register XRASTER	*xr;
 	if (xr->pixels == NULL)
 		return;
 	XFreeColors(xr->pixels, xr->ncolors, 0);
-	free((char *)xr->pixels);
+	free((void *)xr->pixels);
 	xr->pixels = NULL;
 }
 
@@ -107,12 +156,12 @@ register XRASTER	*xr;
 	free_rpixmap(xr);
 	if (xr->ncolors > 0) {
 		unmap_rcolors(xr);
-		free((char *)xr->data.bz);
-		free((char *)xr->pmap);
-		free((char *)xr->cdefs);
+		free((void *)xr->data.bz);
+		free((void *)xr->pmap);
+		free((void *)xr->cdefs);
 	} else
-		free((char *)xr->data.m);
-	free((char *)xr);
+		free((void *)xr->data.m);
+	free((void *)xr);
 }
 
 
