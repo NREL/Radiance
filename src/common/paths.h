1c6d492f8e9ac1ccd174d02c534103a0face82c0
schorsch <schorsch>
2003-06-08 12:01:52 +0000

    Cross-platform functions to find a writeable directory for tempfiles, create unique filenames therein, and create a file.

--- src/common/paths.h
+++ src/common/paths.h
@@ -10,18 +10,42 @@ extern "C" {
 
 #include "copyright.h"
 
+#include <stdlib.h>
+#include <string.h>
+#ifdef _WIN32
+ #include <io.h>
+ #define access _access
+ #define PATH_MAX _MAX_PATH
+#else /* _WIN32 */
+ #include <unistd.h>
+ #include <sys/param.h>
+#endif /* _WIN32 */
+
 #ifdef _WIN32
 
 #define DIRSEP		'/'
 #define ISDIRSEP(c)	((c)=='/' || (c)=='\\')
+#define ISABS(s)	((s)!=NULL \
+        && (s[0])!='\0' \
+        && (   ISDIRSEP(s[0]) \
+            || (   (s[1])!='\0' \
+                && (isupper(s[0])||islower(s[0])) \
+                && (s[1])==':')))
 #define CASEDIRSEP	case '/': case '\\'
 #define PATHSEP		';'
 #define MAXPATH		128
+#define DEFAULT_TEMPDIRS {"C:/TMP", "C:/TEMP", ".", NULL}
 #define TEMPLATE	"rtXXXXXX"
 #define TEMPLEN		8
 #define ULIBVAR		"RAYPATH"
 #ifndef DEFPATH
 #define DEFPATH		";c:/ray/lib"
+#endif
+/* <io.h> only does half the work for access() */
+#ifndef F_OK
+#define  F_OK 00
+#define  W_OK 02
+#define  R_OK 04
 #endif
 extern char  *fixargv0();
 
@@ -29,8 +53,10 @@ extern char  *fixargv0();
 #ifdef AMIGA
 
 #define DIRSEP		'/'
+#define ISABS(s) ((s)!=NULL && (ISDIRSEP(s[0])))
 #define PATHSEP		';'
 #define MAXPATH		128
+#define DEFAULT_TEMPDIRS {"/var/tmp", "/usr/tmp", "/tmp", ".", NULL}
 #define TEMPLATE	"/tmp/rtXXXXXX"
 #define TEMPLEN		13
 #define ULIBVAR		"RAYPATH"
@@ -42,8 +68,10 @@ extern char  *fixargv0();
 #else
 
 #define DIRSEP		'/'
+#define ISABS(s) ((s)!=NULL && (ISDIRSEP(s[0])))
 #define PATHSEP		':'
 #define MAXPATH		256
+#define DEFAULT_TEMPDIRS {"/var/tmp", "/usr/tmp", "/tmp", ".", NULL}
 #define TEMPLATE	"/usr/tmp/rtXXXXXX"
 #define TEMPLEN		17
 #define ULIBVAR		"RAYPATH"
@@ -70,6 +98,26 @@ extern char  *getwd();
 extern char  *getcwd();
 #define  getwd(p)	getcwd(p, sizeof(p))
 #endif
+
+
+/* Find a writeable directory for temporary files */
+/* If s is NULL, we return a static string */
+extern char *temp_directory(char *s, size_t len);
+
+/* Compose a *currently* unique name within a temporary directory */
+/* If s is NULL, we return a static string */
+/* If templ is NULL, we take our default template */
+/* WARNING: On Windows, there's a maximum of 27 unique names within
+            one process for the same template. */
+extern char *temp_filename(char *s, size_t len, char *templ);
+
+/* Same as above, but also open the file and return the descriptor */
+/* This one is supposed to protect against race conditions on unix */
+/* WARNING: On Windows, there's no protection against race conditions */
+extern int temp_file(char *s, size_t len, char *templ);
+
+/* Concatenate two strings, leaving exactly one DIRSEP in between */
+extern char *append_filepath(char *s1, char *s2, size_t len);
 
 
 #ifdef __cplusplus
