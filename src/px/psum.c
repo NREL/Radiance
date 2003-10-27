4ad8733c79a9c22e190e738ec8f05e2afa7d3eb7
Georg Mischler <schorsch@schorsch.com>
2003-10-27 10:22:27 +0000

    Fixed call to getheader() with wrong number of arguments.

--- src/px/psum.c
+++ src/px/psum.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static const char	RCSid[] = "$Id: psum.c,v 2.5 2003/02/22 02:07:27 greg Exp $";
+static const char	RCSid[] = "$Id: psum.c,v 2.6 2003/10/27 10:22:27 schorsch Exp $";
 #endif
 /*
  *  psum.c - program to sum pictures.
@@ -8,10 +8,11 @@ static const char	RCSid[] = "$Id: psum.c,v 2.5 2003/02/22 02:07:27 greg Exp $";
  */
 
 #include  <stdio.h>
-
 #include  <math.h>
 
 #include  "color.h"
+#include  "resolu.h"
+#include  "rterror.h"
 
 
 #define  MAXFILE	8
@@ -94,7 +95,7 @@ char  *argv[];
 						/* get header */
 		fputs(fname[nfile], stdout);
 		fputs(":\n", stdout);
-		getheader(fptr[nfile], tabputs);
+		getheader(fptr[nfile], tabputs, NULL);
 						/* get picture size */
 		if (fgetresolu(&xres, &yres, fptr[nfile]) != (YMAJOR|YDECR)) {
 			fprintf(stderr, "%s: bad picture size\n", progname);
