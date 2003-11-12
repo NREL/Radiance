03212a4c83b8678319851804094fa8657b482f6d
Gregory J. Ward <gregoryjward@gmail.com>
2003-11-12 18:06:54 +0000

    Update to library replacement function frexp()

--- src/common/frexp.c
+++ src/common/frexp.c
@@ -1,10 +1,10 @@
 #ifndef lint
-static const char	RCSid[] = "$Id: frexp.c,v 1.3 2003/02/25 02:47:21 greg Exp $";
+static const char	RCSid[] = "$Id: frexp.c,v 1.4 2003/11/12 18:06:54 greg Exp $";
 #endif
-#include "copyright.h"
 
+double
 frexp(x, ip)		/* call it paranoia, I've seen the lib version */
-register double  x;
+double  x;
 int  *ip;
 {
 	int  neg;
@@ -21,10 +21,7 @@ int  *ip;
 			x *= 2.0;
 	else
 		for (i = 0; x >= 1.0; i++)
-			x /= 2.0;
+			x *= 0.5;
 	*ip = i;
-	if (neg)
-		return(-x);
-	else
-		return(x);
+	return (neg ? -x : x);
 }
