a63f1c892081f3445b9d71c7ba62b9532295590f
greg <greg>
2004-03-02 01:13:45 +0000

    Fixed bug in last change to allow aliased virtual source materials

--- src/rt/virtuals.c
+++ src/rt/virtuals.c
@@ -124,6 +124,8 @@ OBJREC  *o;
 
 	i = o->omod;
 	m = findmaterial(objptr(i));
+	if (m == NULL)
+		return(objptr(i));
 	if (m->otype != MAT_ILLUM || m->oargs.nsargs < 1 ||
 			!strcmp(m->oargs.sarg[0], VOIDID) ||
 			(i = lastmod(objndx(m), m->oargs.sarg[0])) == OVOID)
