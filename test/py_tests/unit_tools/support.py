9a33b6673b8cfc20ef2a6d9b38d82cc258c8dc5d
Georg Mischler <schorsch@schorsch.com>
2004-07-04 12:05:33 +0000

    minor fixes.

--- test/py_tests/unit_tools/support.py
+++ test/py_tests/unit_tools/support.py
@@ -1,26 +1,18 @@
 
 import os
+import sys
 import string
 
 import unittest
 
 def _find_raydir():
-	try: thisfile = __file__
-	except NameError:
-		import support
-		thisfile = support.__file__
-	sdirn = string.count(thisfile, os.sep)
-	if sdirn in [0, 1]:
-		raydir = os.path.join('..','..')
-	elif sdirn == 2:
-		raydir = os.path.join('..')
-	elif sdirn == 3:
-		raydir = '.'
-	else:
-		unit_tools = os.path.split(thisfile)[0]
-		py_tests = os.path.split(unit_tools)[0]
-		test = os.path.split(py_tests)[0]
-		raydir = os.path.split(test)[0]
+	if __name__ == '__main__':
+		thisfile = sys.argv[0]
+	else: thisfile = sys.modules[__name__].__file__
+	unit_tools = os.path.abspath(os.path.dirname(thisfile))
+	py_tests = os.path.dirname(unit_tools)
+	test = os.path.dirname(py_tests)
+	raydir = os.path.dirname(test)
 	return raydir
 
 RAYDIR = _find_raydir()
@@ -28,13 +20,13 @@ DATADIR = os.path.join(RAYDIR, 'test', 'test data')
 BINDIR = os.path.join(RAYDIR, 'bin')
 
 def binfile(fn):
-	'''return the full/relative path to file in bin dir'''
+	'''return the full path to file in bin dir'''
 	if os.name == 'nt':
 		return os.path.normpath(os.path.join(BINDIR, fn + '.exe'))
 	else: return os.path.normpath(os.path.join(BINDIR, fn))
 
 def datafile(fn):
-	'''return the full/relative path to file in data dir'''
+	'''return the full path to file in data dir'''
 	return os.path.normpath(os.path.join(DATADIR, fn))
 
 def run_case(c):
