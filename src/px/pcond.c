5aa78446fee4bb4639201c75707d5c6a0f774e7a
greg <greg>
2004-11-14 22:19:50 +0000

    Increased minimum view angle from 3 to 5 degrees

--- src/px/pcond.c
+++ src/px/pcond.c
@@ -261,7 +261,7 @@ getahead(void)			/* load picture header */
 		exit(1);
 	}
 	if (!gotview || ourview.type == VT_PAR ||
-			ourview.horiz <= 3. | ourview.vert <= 3.) {
+			ourview.horiz <= 5. | ourview.vert <= 5.) {
 		ourview = stdview;
 		ourview.type = VT_PER;
 		if (pixaspect*inpres.yr < inpres.xr) {
