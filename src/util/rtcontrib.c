3098185ac1cbc857abd3f609ae0cd762891f6867
greg <greg>
2005-09-13 22:58:22 +0000

    Added check for no arguments

--- src/util/rtcontrib.c
+++ src/util/rtcontrib.c
@@ -173,6 +173,13 @@ main(int argc, char *argv[])
 	char	*binval = NULL;
 	char	fmt[8];
 	int	i, j;
+				/* need at least one argument */
+	if (argc < 2) {
+		fprintf(stderr,
+"Usage: %s [-n nprocs][-r][-e expr][-f source][-o ospec][-b binv] {-m mod | -M file} [rtrace options] octree\n",
+			argv[0]);
+		exit(1);
+	}
 				/* global program name */
 	gargv = argv;
 				/* initialize calcomp routines */
