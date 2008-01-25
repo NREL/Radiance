7b9b4beaf3173de560a632014a926baf7898166b
Gregory J. Ward <gregoryjward@gmail.com>
2008-01-25 02:11:12 +0000

    Fixed bug in rotation computation when X' is aligned with Z-axis

--- src/gen/mkillum4.c
+++ src/gen/mkillum4.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static const char RCSid[] = "$Id: mkillum4.c,v 2.8 2007/12/13 07:03:37 greg Exp $";
+static const char RCSid[] = "$Id: mkillum4.c,v 2.9 2008/01/25 02:11:13 greg Exp $";
 #endif
 /*
  * Routines for handling BSDF data within mkillum
@@ -377,6 +377,16 @@ addrot(			/* compute rotation (x,y,z) => (xp,yp,zp) */
 	char	**xfp = xfarg;
 	double	theta;
 
+	if (yp[2]*yp[2] + zp[2]*zp[2] < 2.*FTINY*FTINY) {
+		/* Special case for X' along Z-axis */
+		theta = -atan2(yp[0], yp[1]);
+		*xfp++ = "-ry";
+		*xfp++ = xp[2] < 0.0 ? "90" : "-90";
+		*xfp++ = "-rz";
+		sprintf(bufs[bn], "%f", theta*(180./PI));
+		*xfp++ = bufs[bn++];
+		return(xfp - xfarg);
+	}
 	theta = atan2(yp[2], zp[2]);
 	if (!FEQ(theta,0.0)) {
 		*xfp++ = "-rx";
--- src/gen/replmarks.c
+++ src/gen/replmarks.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static const char RCSid[] = "$Id: replmarks.c,v 2.14 2005/12/28 18:35:42 greg Exp $";
+static const char RCSid[] = "$Id: replmarks.c,v 2.15 2008/01/25 02:11:13 greg Exp $";
 #endif
 /*
  * Replace markers in Radiance scene description with objects or instances.
@@ -391,6 +391,14 @@ addrot(		/* compute rotation (x,y,z) => (xp,yp,zp) */
 	int	n;
 	double	theta;
 
+	if (yp[2]*yp[2] + zp[2]*zp[2] < 2.*FTINY*FTINY) {
+		/* Special case for X' along Z-axis */
+		theta = -atan2(yp[0], yp[1]);
+		sprintf(xf, " -ry %f -rz %f",
+				xp[2] < 0.0 ? 90.0 : -90.0,
+				theta*(180./PI));
+		return(4);
+	}
 	n = 0;
 	theta = atan2(yp[2], zp[2]);
 	if (!FEQ(theta,0.0)) {
