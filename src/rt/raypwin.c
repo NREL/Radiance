6dc1ab318a3e2a14938b2565ad5331fbefaf8669
greg <greg>
2008-04-17 18:48:09 +0000

    Bug fixes

--- src/rt/raypwin.c
+++ src/rt/raypwin.c
@@ -55,6 +55,11 @@ ray_pqueue(			/* queue a ray for computation */
 {
 	if (r == NULL)
 		return(0);
+	if (ray_pnidle <= 0) {
+		RAY	new_ray = *r;
+		*r = queued_ray;
+		queued_ray = new_ray;
+	}
 	samplendx++;
 	rayvalue(r);
 	return(1);
@@ -70,9 +75,9 @@ ray_presult(		/* check for a completed ray */
 	if (r == NULL)
 		return(0);
 	if (ray_pnidle <= 0) {
+		*r = queued_ray;
 		samplendx++;
 		rayvalue(r);
-		*r = queued_ray;
 		ray_pnidle = 1;
 		return(1);
 	}
