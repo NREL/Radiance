e371fd89ff3b0e84030fe29672a5bcf7c362ccec
Gregory J. Ward <gregoryjward@gmail.com>
2009-04-18 16:14:26 +0000

    Corrected wording on default value

--- src/gen/mkillum.c
+++ src/gen/mkillum.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static const char RCSid[] = "$Id: mkillum.c,v 2.33 2007/12/13 07:03:37 greg Exp $";
+static const char RCSid[] = "$Id: mkillum.c,v 2.34 2009/04/18 16:14:26 greg Exp $";
 #endif
 /*
  * Make illum sources for optimizing rendering process
@@ -444,8 +444,8 @@ printopts(void)			/* print out option default values */
 		printf("l+\t\t\t\t# light type on\n");
 	else
 		printf("l-\t\t\t\t# light type off\n");
-	printf("d=%d\t\t\t\t# density of points\n", thisillum.sampdens);
-	printf("s=%d\t\t\t\t# samples per point\n", thisillum.nsamps);
+	printf("d=%d\t\t\t\t# density of directions\n", thisillum.sampdens);
+	printf("s=%d\t\t\t\t# samples per direction\n", thisillum.nsamps);
 	printf("b=%f\t\t\t# minimum average brightness\n", thisillum.minbrt);
 	switch (thisillum.udir) {
 	case UDzneg:
