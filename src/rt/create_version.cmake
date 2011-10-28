d1e02e1224e23262e0c0cbb9baf56578f501c931
Gregory J. Ward <gregoryjward@gmail.com>
2011-10-28 20:46:47 +0000

    Fixes to Version.c update in cmake build system

--- src/rt/create_version.cmake
+++ src/rt/create_version.cmake
@@ -1,4 +1,11 @@
-#${CMAKE_COMMAND} -DVERSION_OUT_FILE=v  -DVERSION_IN_FILE=src/rt/VERSION -P src/common/create_version.cmake
+#${CMAKE_COMMAND} -DVERSION_OUT_FILE=v  -DVERSION_IN_FILE=src/rt/VERSION -DVERSION_GOLD=src/rt/Version.c -P src/common/create_version.cmake
+
+# if the gold version exists then use that instead
+if(EXISTS "${VERSION_GOLD}")
+  configure_file("${VERSION_GOLD}" "${VERSION_OUT_FILE}" COPYONLY)
+  return()
+endif()
+
 find_program(DATE date)
 if(DATE)
   execute_process(COMMAND ${DATE} OUTPUT_VARIABLE DATE_STR)
