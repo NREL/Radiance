146449d1e75a60789e0341819c740540ebd0fabc
Gregory J. Ward <gregoryjward@gmail.com>
2011-12-01 18:47:35 +0000

    Increased significant digits for intercept

--- src/meta/gcalc.c
+++ src/meta/gcalc.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static const char	RCSid[] = "$Id: gcalc.c,v 1.3 2003/11/15 02:13:37 schorsch Exp $";
+static const char	RCSid[] = "$Id: gcalc.c,v 1.4 2011/12/01 18:47:35 greg Exp $";
 #endif
 /*
  *  gcalc.c - routines to do calculations on graph files.
@@ -71,7 +71,7 @@ gcheader(			/* print header */
 			printf("|___Integ_");
 			break;
 		case 'l':
-			printf("|___Slope_____Intcp______Corr_");
+			printf("|____Slope_______Intcp____Corr_");
 			break;
 		default:
 			break;
@@ -113,7 +113,7 @@ gcvalue(		/* print the values for the curve */
 			d1 = (xysum - xsum*ysum/npts)/d3;
 			d2 = (ysum - d1*xsum)/npts;
 			d3 = d1*sqrt(d3/(yysum - ysum*ysum/npts));
-			printf(" %9.5f %9.2f %9.5f", d1, d2, d3);
+			printf(" %10.6f %11.4f %7.3f", d1, d2, d3);
 			break;
 		default:
 			break;
