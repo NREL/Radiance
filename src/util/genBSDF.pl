0460e5dd5b3413bfef87bebb761f299e76a44d7e
greg <greg>
2012-03-15 02:32:19 +0000

    Fix for -t3 N -t -1

--- src/util/genBSDF.pl
+++ src/util/genBSDF.pl
@@ -347,6 +347,9 @@ if ($pctcull >= 0) {
 	$cmd .= "$td/" . ($bmodnm,$fmodnm)[$forw] . "_???.flt";
 	print "{\n";
 	system "$cmd" || die "Failure running rcalc";
+	for (my $i = ($tensortree==3)*$ns*$ns*$ns/2; $i-- > 0; ) {
+		print "0\n";
+	}
 	print "}\n";
 }
 print
@@ -380,6 +383,9 @@ if ($pctcull >= 0) {
 	$cmd .= "$td/" . ($fmodnm,$bmodnm)[$forw] . "_???.flt";
 	print "{\n";
 	system "$cmd" || die "Failure running rcalc";
+	for (my $i = ($tensortree==3)*$ns*$ns*$ns/2; $i-- > 0; ) {
+		print "0\n";
+	}
 	print "}\n";
 }
 print
