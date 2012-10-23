e0b6d7a69d447e124f05300102a6169a3400519e
greg <greg>
2012-10-23 05:03:52 +0000

    Minor optimization

--- src/rt/o_mesh.c
+++ src/rt/o_mesh.c
@@ -113,9 +113,10 @@ RAY	*r;
 				"missing triangle vertices in mesh_hit");
 		sv1 = volume_sign(r, tvi[0], tvi[1]);
 		sv2 = volume_sign(r, tvi[1], tvi[2]);
+		if (sv1 != sv2)			/* compare volume signs */
+			continue;
 		sv3 = volume_sign(r, tvi[2], tvi[0]);
-						/* compare volume signs */
-		if ((sv1 != sv2) | (sv2 != sv3))
+		if (sv2 != sv3)
 			continue;
 						/* compute intersection */
 		getmeshvert(&tv[0], curmsh, tvi[0], MT_V);
