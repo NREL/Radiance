9590db921db2e35a0c2c9bf0ac78cd26c4456743
greg <greg>
2013-01-11 17:54:17 +0000

    Minor fix in unreachable error

--- src/util/dctimestep.c
+++ src/util/dctimestep.c
@@ -122,7 +122,7 @@ cm_load(const char *fname, int nrows, int ncols, int dtype)
 	flockfile(fp);
 #endif
 	if (dtype != DTascii)
-		SET_FILE_BINARY(fp);
+		SET_FILE_BINARY(fp);		/* doesn't really work */
 	if (dtype == DTfromHeader)
 		dtype = getDTfromHeader(fp);
 	switch (dtype) {
@@ -266,7 +266,7 @@ cm_column(const CMATRIX *cm, int c)
 	int	dr;
 
 	if ((c < 0) | (c >= cm->ncols))
-		return(NULL);
+		error(INTERNAL, "column requested outside matrix");
 	cvr = cm_alloc(cm->nrows, 1);
 	if (cvr == NULL)
 		return(NULL);
