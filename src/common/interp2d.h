40f7445c63ec229456b48a37522ef3a109cb8d06
Gregory J. Ward <gregoryjward@gmail.com>
2013-02-09 00:55:40 +0000

    Created routines for interpolating anisotropic 2-D samples

---
+++ src/common/interp2d.h
@@ -0,0 +1,32 @@
+/* RCSid $Id: interp2d.h,v 2.1 2013/02/09 00:55:40 greg Exp $ */
+/*
+ * Header for interpolation of anisotropic values on 2-D plane.
+ *
+ *	G.Ward Feb 2013
+ */
+
+#define	NI2DSMF	0.42f			/* minimal smoothing radius */
+
+#define NI2DIR	(2*4)			/* # interpolation directions */
+
+/* Data structure for interpolant */
+typedef struct {
+	int		ns;		/* number of sample positions */
+	float		rmin;		/* minimum valid radius (default=.5) */
+	float		smf;		/* smoothing factor (def=NI2DSMF) */
+	unsigned short	(*ra)[NI2DIR];	/* anisotropic radii (private) */
+	float		spt[1][2];	/* sample positions (extends struct) */
+} INTERP2;
+
+/* Allocate a new set of interpolation samples (caller assigns spt[] array) */
+extern INTERP2	*interp2_alloc(int nsamps);
+
+/* Assign full set of normalized weights to interpolate the given location */
+extern int	interp2_weights(float wtv[], INTERP2 *ip, double x, double y);
+
+/* Get normalized weights and indexes for n best samples in descending order */
+extern int	interp2_topsamp(float wt[], int si[], const int n,
+					INTERP2 *ip, double x, double y);
+
+/* Free interpolant */
+extern void	interp2_free(INTERP2 *ip);
