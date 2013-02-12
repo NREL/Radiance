ae3ee1055dda21a2da1391dea2a38a58bb6f9304
Gregory J. Ward <gregoryjward@gmail.com>
2013-02-12 17:47:58 +0000

    Really minor tweaks

--- src/common/interp2d.c
+++ src/common/interp2d.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static const char RCSid[] = "$Id: interp2d.c,v 2.6 2013/02/12 02:56:05 greg Exp $";
+static const char RCSid[] = "$Id: interp2d.c,v 2.7 2013/02/12 17:47:58 greg Exp $";
 #endif
 /*
  * General interpolation method for unstructured values on 2-D plane.
@@ -17,7 +17,7 @@ static const char RCSid[] = "$Id: interp2d.c,v 2.6 2013/02/12 02:56:05 greg Exp 
  * each of NI2DIR surrounding directions.  To speed this
  * calculation, we sort the data into half-planes and apply
  * simple tests to see which neighbor is closest in each
- * direction.  Once we have our approximate neighborhood
+ * angular slice.  Once we have our approximate neighborhood
  * for a sample, we can use it in a modified Gaussian weighting
  * with allowing local anisotropy.  Harmonic weighting is added
  * to reduce the influence of distant neighbors.  This yields a
@@ -90,7 +90,7 @@ interp2_spacing(INTERP2 *ip, double mind)
 {
 	if (mind <= 0)
 		return;
-	if ((.998*ip->dmin <= mind) && (mind <= 1.002*ip->dmin))
+	if ((.998*ip->dmin <= mind) & (mind <= 1.002*ip->dmin))
 		return;
 	if (ip->da != NULL) {	/* will need to recompute distribution */
 		free(ip->da);
