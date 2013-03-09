f5b57e85f90af114e736cec8da942f50012ef827
Gregory J. Ward <gregoryjward@gmail.com>
2013-03-09 18:53:05 +0000

    Slight efficiency improvement and ansification

--- src/common/modobject.c
+++ src/common/modobject.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static const char RCSid[] = "$Id: modobject.c,v 2.12 2004/07/14 02:37:23 greg Exp $";
+static const char RCSid[] = "$Id: modobject.c,v 2.13 2013/03/09 18:53:05 greg Exp $";
 #endif
 /*
  *  Routines for tracking object modifiers
@@ -25,10 +25,11 @@ static int  otndx(char *, struct ohtab *);
 
 
 OBJECT
-objndx(op)			/* get object number from pointer */
-register OBJREC  *op;
+objndx(				/* get object number from pointer */
+	OBJREC  *op
+)
 {
-	register int  i, j;
+	int  i, j;
 
 	for (i = nobjects>>OBJBLKSHFT; i >= 0; i--) {
 		j = op - objblock[i];
@@ -40,19 +41,21 @@ register OBJREC  *op;
 
 
 OBJECT
-lastmod(obj, mname)		/* find modifier definition before obj */
-OBJECT  obj;
-char  *mname;
+lastmod(			/* find modifier definition before obj */
+	OBJECT  obj,
+	char  *mname
+)
 {
-	register OBJREC  *op;
-	register int  i;
+	OBJREC  *op;
+	int  i;
 
 	i = modifier(mname);		/* try hash table first */
 	if ((obj == OVOID) | (i < obj))
 		return(i);
 	for (i = obj; i-- > 0; ) {	/* need to search */
 		op = objptr(i);
-		if (ismodifier(op->otype) && !strcmp(op->oname, mname))
+		if (ismodifier(op->otype) && op->oname[0] == mname[0] &&
+					!strcmp(op->oname, mname))
 			return(i);
 	}
 	return(OVOID);
@@ -60,10 +63,11 @@ char  *mname;
 
 
 OBJECT
-modifier(mname)			/* get a modifier number from its name */
-char  *mname;
+modifier(			/* get a modifier number from its name */
+	char  *mname
+)
 {
-	register int  ndx;
+	int  ndx;
 
 	ndx = otndx(mname, &modtab);
 	return(modtab.htab[ndx]);
@@ -72,10 +76,11 @@ char  *mname;
 
 #ifdef  GETOBJ
 OBJECT
-object(oname)			/* get an object number from its name */
-char  *oname;
+object(				/* get an object number from its name */
+	char  *oname
+)
 {
-	register int  ndx;
+	int  ndx;
 
 	ndx = otndx(oname, &objtab);
 	return(objtab.htab[ndx]);
@@ -84,10 +89,11 @@ char  *oname;
 
 
 void
-insertobject(obj)		/* insert new object into our list */
-register OBJECT  obj;
+insertobject(			/* insert new object into our list */
+	OBJECT  obj
+)
 {
-	register int  i;
+	int  i;
 
 	if (ismodifier(objptr(obj)->otype)) {
 		i = otndx(objptr(obj)->oname, &modtab);
@@ -105,7 +111,7 @@ register OBJECT  obj;
 
 
 void
-clearobjndx()			/* clear object hash tables */
+clearobjndx(void)		/* clear object hash tables */
 {
 	if (modtab.htab != NULL) {
 		free((void *)modtab.htab);
@@ -121,13 +127,14 @@ clearobjndx()			/* clear object hash tables */
 
 
 static int
-nexthsiz(oldsiz)		/* return next hash table size */
-int  oldsiz;
+nexthsiz(			/* return next hash table size */
+	int  oldsiz
+)
 {
 	static int  hsiztab[] = {
 		251, 509, 1021, 2039, 4093, 8191, 16381, 0
 	};
-	register int  *hsp;
+	int  *hsp;
 
 	for (hsp = hsiztab; *hsp; hsp++)
 		if (*hsp > oldsiz)
@@ -137,13 +144,14 @@ int  oldsiz;
 
 
 static int
-otndx(name, tab)		/* get object table index for name */
-char  *name;
-register struct ohtab  *tab;
+otndx(				/* get object table index for name */
+	char  *name,
+	struct ohtab  *tab
+)
 {
 	OBJECT  *oldhtab;
 	int  hval, i;
-	register int  ndx;
+	int  ndx;
 
 	if (tab->htab == NULL) {		/* new table */
 		tab->hsiz = nexthsiz(tab->hsiz);
