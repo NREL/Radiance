5c70616b74395c129a05a1d63b44b102c7c95ccf
greg <greg>
2013-09-12 16:09:31 +0000

    Fixed brittle parser that was not recognizing format from different systems

--- src/cv/pabopto2bsdf.c
+++ src/cv/pabopto2bsdf.c
@@ -62,19 +62,22 @@ init_pabopto_inp(const int i, const char *fname)
 	inpfile[i].theta = inpfile[i].phi = -10001.;
 				/* read header information */
 	while ((c = getc(fp)) == '#' || c == EOF) {
+		char	typ[32];
 		if (fgets(buf, sizeof(buf), fp) == NULL) {
 			fputs(fname, stderr);
 			fputs(": unexpected EOF\n", stderr);
 			fclose(fp);
 			return(0);
 		}
-		if (!strcmp(buf, "format: theta phi DSF\n")) {
-			inpfile[i].isDSF = 1;
-			continue;
-		}
-		if (!strcmp(buf, "format: theta phi BSDF\n")) {
-			inpfile[i].isDSF = 0;
-			continue;
+		if (sscanf(buf, "format: theta phi %s", typ) == 1) {
+			if (!strcasecmp(typ, "DSF")) {
+				inpfile[i].isDSF = 1;
+				continue;
+			}
+			if (!strcasecmp(typ, "BSDF")) {
+				inpfile[i].isDSF = 0;
+				continue;
+			}
 		}
 		if (sscanf(buf, "intheta %lf", &inpfile[i].theta) == 1)
 			continue;
