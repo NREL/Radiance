93f46c6255f0e39aabd1853017411587b33ddfb0
greg <greg>
2014-01-31 00:47:10 +0000

    Moved misplaced assignment

--- src/cv/pabopto2bsdf.c
+++ src/cv/pabopto2bsdf.c
@@ -219,9 +219,9 @@ main(int argc, char *argv[])
 #ifdef DEBUG
 	fprintf(stderr, "Minimum BSDF = %.4f\n", bsdf_min);
 #endif
+	min_log = log(bsdf_min*.5);
 #if 1						/* produce spheres at meas. */
 	puts("void plastic yellow\n0\n0\n5 .6 .4 .01 .04 .08\n");
-	min_log = log(bsdf_min*.5);
 	n = 0;
 	for (i = 0; i < GRIDRES; i++)
 	    for (j = 0; j < GRIDRES; j++)
