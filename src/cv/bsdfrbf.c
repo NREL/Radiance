07c8f0fb1659163f84e754dc1f728528ddd0b120
greg <greg>
2014-02-18 16:42:16 +0000

    Reduced error thresholds to improve lobe sampling

--- src/cv/bsdfrbf.c
+++ src/cv/bsdfrbf.c
@@ -32,10 +32,10 @@ static const char RCSid[] = "$Id$";
 #define RSCA		2.2		/* radius scaling factor (empirical) */
 #endif
 #ifndef SMOOTH_MSE
-#define SMOOTH_MSE	5e-5		/* acceptable mean squared error */
+#define SMOOTH_MSE	2e-5		/* acceptable mean squared error */
 #endif
 #ifndef SMOOTH_MSER
-#define SMOOTH_MSER	0.07		/* acceptable relative MSE */
+#define SMOOTH_MSER	0.03		/* acceptable relative MSE */
 #endif
 #define MAX_RAD		(GRIDRES/8)	/* maximum lobe radius */
 
