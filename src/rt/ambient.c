33c99c9753d486de55e029b76f9607973c4967ae
greg <greg>
2014-04-19 19:20:47 +0000

    Tidying up code, bug fixes and minor improvements

--- src/rt/ambient.c
+++ src/rt/ambient.c
@@ -398,7 +398,7 @@ sumambient(		/* get interpolated ambient value */
 		delta_t2 = d*d;
 		d = DOT(ck0, uvw[1]) / av->rad[1];
 		delta_t2 += d*d;
-		if (delta_t2 >= ambacc*ambacc)
+		if (delta_t2 >= qambacc*qambacc)
 			continue;
 		/*
 		 *  Intersection behind test
@@ -413,7 +413,7 @@ sumambient(		/* get interpolated ambient value */
 		 */
 		extambient(ct, av, r->rop, rn, uvw);
 		d = tfunc(maxangle, sqrt(delta_r2), 0.0) *
-			tfunc(ambacc, sqrt(delta_t2), 0.0);
+			tfunc(qambacc, sqrt(delta_t2), 0.0);
 		scalecolor(ct, d);
 		addcolor(acol, ct);
 		wsum += d;
