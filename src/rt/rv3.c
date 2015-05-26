572e545847a7f64a03a8fef5c12f01d2c5f47859
greg <greg>
2015-05-26 10:00:47 +0000

    Added photon map support to rad

--- src/rt/rv3.c
+++ src/rt/rv3.c
@@ -224,9 +224,9 @@ paint(			/* compute and paint a rectangle */
 		} else if (ambounce == 0)
 			flushintvl = ray_pnprocs*WFLUSH;
 		else if (niflush < WFLUSH)
-			flushintvl = ray_pnprocs*niflush/(ambounce+1);
+			flushintvl = ray_pnprocs*niflush/(ambounce*(ambounce>0)+1);
 		else
-			flushintvl = ray_pnprocs*WFLUSH/(ambounce+1);
+			flushintvl = ray_pnprocs*WFLUSH/(ambounce*(ambounce>0)+1);
 		if (lastflush > counter)
 			lastflush = 0;		/* counter wrapped */
 
