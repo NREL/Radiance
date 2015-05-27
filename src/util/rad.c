c417bb811c9f451f428098d4e462b26ec4e2f855
greg <greg>
2015-05-27 14:22:02 +0000

    Need to remove photon maps prior to updating

--- src/util/rad.c
+++ src/util/rad.c
@@ -657,12 +657,14 @@ mkpmap(void)			/* run mkpmap if indicated */
 				badvalue(REPORT);
 		}
 		if (pgmapname != NULL && pgmapdate < oct1date) {
+			unlink(pgmapname);
 			cp = addarg(cp, "-apg");
 			addarg(cp, vval(PGMAP));
 			cp = sskip(sskip(cp));	/* remove any bandwidth */
 			*cp = '\0';
 		}
 		if (pcmapname != NULL && pcmapdate < oct1date) {
+			unlink(pcmapname);
 			cp = addarg(cp, "-apc");
 			addarg(cp, vval(PCMAP));
 			cp = sskip(sskip(cp));	/* remove any bandwidth */
