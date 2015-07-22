b24432688c49e31dc55d97d8b9e44148113c23c3
Gregory J. Ward <gregoryjward@gmail.com>
2015-07-22 04:29:56 +0000

    Reduced precision of multiplies in favor of time when little differ found

--- src/util/rmatrix.c
+++ src/util/rmatrix.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static const char RCSid[] = "$Id: rmatrix.c,v 2.15 2015/05/08 18:25:03 greg Exp $";
+static const char RCSid[] = "$Id: rmatrix.c,v 2.16 2015/07/22 04:29:56 greg Exp $";
 #endif
 /*
  * General matrix operations.
@@ -514,8 +514,7 @@ rmx_multiply(const RMATRIX *m1, const RMATRIX *m2)
 	        for (k = mres->ncomp; k--; ) {
 		    long double	d = 0;
 		    for (h = m1->ncols; h--; )
-			d += (long double)rmx_lval(m1,i,h,k) *
-				(long double)rmx_lval(m2,h,j,k);
+			d += rmx_lval(m1,i,h,k) * rmx_lval(m2,h,j,k);
 		    rmx_lval(mres,i,j,k) = (double)d;
 		}
 	return(mres);
