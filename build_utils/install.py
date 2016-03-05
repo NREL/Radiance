387970ecd13d5b37ae68ba947a40b068cb95e633
Georg Mischler <schorsch@schorsch.com>
2016-03-05 13:24:58 +0000

    SCons build now fully functional again on linux (SCons 2.3.0, Python 2.7)

--- build_utils/install.py
+++ build_utils/install.py
@@ -41,3 +41,21 @@ def install_script(target, source, env):
 		tf.write(sf.read())
 		os.chmod(str(t), 00755)
 
+def install_tclscript(target, source, env):
+	# XXX posix only for the moment
+	for t,s in map(None, target,source):
+		bindir = os.path.split(t.get_abspath())[0]
+		instdir = os.path.split(bindir)[0]
+		tcllibdir = os.path.join(instdir, 'lib', 'tcl')
+		sf = open(str(s), 'r')
+		tf = open(str(t), 'w')
+		# as recommended in the wish manpage
+		tf.write('''#!/bin/sh\n''')
+		for line in sf.readlines():
+			if line.startswith('#!'):
+				tf.write('### ' + line)
+			if line.startswith('set radlib'):
+				# XXX this should really be handled by an envvar.
+				line = 'set radlib %s\n' % tcllibdir
+			tf.write(line)
+		os.chmod(str(t), 00755)
