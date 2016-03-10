eb48a0e86d0b762dda38c5e9e2b57269ac65e26a
schorsch <schorsch>
2016-03-10 18:09:43 +0000

    MS VC 2015 (possibly earler?) has learned about erf() and erfc()

--- src/common/rtmath.h
+++ src/common/rtmath.h
@@ -46,7 +46,9 @@ extern double	atan2a(double y, double x);
 #define  ttan(x)		(tsin(x)/tcos(x))
 #endif
 
-#if defined(_WIN32) || defined(_WIN64)
+/* VC 2015 has those defined (XXX possibly earlier?) */
+#if ((defined(_WIN32) || defined(_WIN64))
+		&& (!defined(_MS_VER) || _MS_VER < 1900))
 extern double	erf(double x);
 extern double	erfc(double x);
 #endif
