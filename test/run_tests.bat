4543067ca10e0f69f7824759a63eb9ed864a1b60
Georg Mischler <schorsch@schorsch.com>
2016-04-01 22:07:55 +0000

    data files for test_falsecolor

--- test/run_tests.bat
+++ test/run_tests.bat
@@ -2,7 +2,7 @@
 setlocal
 set PATH="c:\program files\python 2.7";%PATH%
 set PYTHONPATH="c:\program files\python 2.7\lib"
-python run_all.py %*
+python run_tests.py %* -V
 if errorlevel 9009 echo you do not have python in your PATH
 REM color 00 causes this script to exit with non-zero exit status
 if errorlevel 1 color 00
