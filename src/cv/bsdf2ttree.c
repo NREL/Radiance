ddd107140a34436f9b125d7ebc27e53485254f19
greg <greg>
2016-10-12 17:36:11 +0000

    Decreased number of super-samples to improve speed

--- src/cv/bsdf2ttree.c
+++ src/cv/bsdf2ttree.c
@@ -27,7 +27,7 @@ static int		samp_order = 6;
 const double		ssamp_thresh = 0.35;
 				/* number of super-samples */
 #ifndef NSSAMP
-#define	NSSAMP		100
+#define	NSSAMP		64
 #endif
 				/* limit on number of RBF lobes */
 static int		lobe_lim = 15000;
