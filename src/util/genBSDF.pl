d968b987b86776f176633fbab99682ab16816474
Gregory J. Ward <gregoryjward@gmail.com>
2017-06-08 15:37:15 +0000

    Fixed typo.

--- src/util/genBSDF.pl
+++ src/util/genBSDF.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -w
-# RCSid $Id: genBSDF.pl,v 2.76 2017/05/31 17:25:21 greg Exp $
+# RCSid $Id: genBSDF.pl,v 2.77 2017/06/08 15:37:15 greg Exp $
 #
 # Compute BSDF based on geometry and material description
 #
@@ -116,7 +116,7 @@ while ($#ARGV >= 0) {
 		shift @ARGV;
 	} elsif ("$ARGV[0]" =~ /^[-+]C/) {
 		$docolor = ("$ARGV[0]" =~ /^\+/);
-	} elsif $("$ARGV[0" =~ /^[-+]a/) {
+	} elsif ("$ARGV[0" =~ /^[-+]a/) {
 		$dorecip = ("$ARGV[0]" =~ /^\+/);
 	} elsif ("$ARGV[0]" =~ /^[-+]f/) {
 		$doforw = ("$ARGV[0]" =~ /^\+/);
