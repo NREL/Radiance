f9ce00d092e68b88c28a0d44df53c7059b4d4939
greg <greg>
2018-01-08 16:51:32 +0000

    Update to CMake build system from Rob G. and Alex S., adding test cases

--- src/rt/create_version.cmake
+++ src/rt/create_version.cmake
@@ -1,4 +1,4 @@
-#${CMAKE_COMMAND} -DVERSION_OUT_FILE=v  -DVERSION_IN_FILE=src/rt/VERSION -DVERSION_GOLD=src/rt/Version.c -P src/common/create_version.cmake
+#${CMAKE_COMMAND} -DRADIANCE_VERSION=v -DVERSION_OUT_FILE=v -DVERSION_IN_FILE=src/rt/VERSION -DVERSION_GOLD=src/rt/Version.c -P src/common/create_version.cmake
 
 # if the gold version exists then use that instead
 if(EXISTS "${VERSION_GOLD}")
@@ -8,26 +8,14 @@ endif()
 
 find_program(DATE date)
 if(DATE)
-  execute_process(COMMAND ${DATE} OUTPUT_VARIABLE DATE_STR)
-  string(STRIP "${DATE_STR}" DATE_STR)
-endif()
-find_program(WHO whoami)
-if(WHO)
-  execute_process(COMMAND ${WHO} OUTPUT_VARIABLE WHO_STR)
-  string(STRIP "${WHO_STR}" WHO_STR)
-endif()
-find_program(HOSTNAME hostname)
-if(HOSTNAME)
-  execute_process(COMMAND ${HOSTNAME} OUTPUT_VARIABLE HOST_STR)
-  message("DATE= ${DATE_STR}")
-  string(STRIP "${HOST_STR}" HOST_STR)
+  execute_process(COMMAND ${DATE} "+%F"
+    OUTPUT_VARIABLE DATE_STR
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
 endif()
 file(READ "${VERSION_IN_FILE}" VERSION)
 string(STRIP "${VERSION}" VERSION)
 message("${VERSION}")
 file(WRITE "${VERSION_OUT_FILE}"
-"char VersionID[]=\"${VERSION} lastmod ${DATE_STR}"
-" by ${WHO_STR} on ${HOST_STR}\";\n"
+  "char VersionID[]=\"RADIANCE ${RADIANCE_VERSION} NREL ${DATE_STR} (based on ${VERSION} Official Release by G. Ward)\";\n"
 )
-
-# todo actually get the date, user name and hostname in this script
