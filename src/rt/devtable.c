24caf01c35ca90967671a68b03b0571ffb5198bd
Gregory J. Ward <gregoryjward@gmail.com>
2018-04-06 17:52:33 +0000

    Made excluding X11 a little simpler

--- src/rt/devtable.c
+++ src/rt/devtable.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static const char	RCSid[] = "$Id: devtable.c,v 2.9 2016/03/19 12:51:36 schorsch Exp $";
+static const char	RCSid[] = "$Id: devtable.c,v 2.10 2018/04/06 17:52:33 greg Exp $";
 #endif
 /*
  *  devtable.c - device table for rview.
@@ -11,10 +11,10 @@ static const char	RCSid[] = "$Id: devtable.c,v 2.9 2016/03/19 12:51:36 schorsch 
 
 #if !defined(HAS_X11) && !defined(HAS_QT) && !defined(WIN_RVIEW)
 /* weird logic ... */
-#define HAS_X11
+#define HAS_X11 1
 #endif
 
-#ifdef HAS_X11
+#if HAS_X11
 extern dr_initf_t x11_init;
 char  dev_default[] = "x11";
 #elif defined(HAS_QT)
@@ -33,7 +33,7 @@ extern dr_initf_t win_rvudev_init;
 
 struct device  devtable[] = {			/* supported devices */
 	{"slave", "Slave driver", slave_init},
-#ifdef HAS_X11
+#if HAS_X11
 	{"x11", "X11 color or greyscale display", x11_init},
 	{"x11d", "X11 display using stdin/stdout", x11_init},
 #endif
