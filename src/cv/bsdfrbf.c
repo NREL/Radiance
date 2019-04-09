dce19aaccb9b471667bfa5c5c1d54e5a582cb2a5
Gregory J. Ward <gregoryjward@gmail.com>
2019-04-09 22:39:33 +0000

    Reduced relative error tolerance to 1/4 what it was after finding issues

--- src/cv/bsdfrbf.c
+++ src/cv/bsdfrbf.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static const char RCSid[] = "$Id: bsdfrbf.c,v 2.31 2018/08/01 17:00:22 greg Exp $";
+static const char RCSid[] = "$Id: bsdfrbf.c,v 2.32 2019/04/09 22:39:33 greg Exp $";
 #endif
 /*
  * Radial basis function representation for BSDF data.
@@ -38,7 +38,7 @@ static const char RCSid[] = "$Id: bsdfrbf.c,v 2.31 2018/08/01 17:00:22 greg Exp 
 #define SMOOTH_MSE	5e-5		/* acceptable mean squared error */
 #endif
 #ifndef SMOOTH_MSER
-#define SMOOTH_MSER	0.01		/* acceptable relative MSE */
+#define SMOOTH_MSER	0.0025		/* acceptable relative MSE */
 #endif
 #define MAX_RAD		(GRIDRES/8)	/* maximum lobe radius */
 
