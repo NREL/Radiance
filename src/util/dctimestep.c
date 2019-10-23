4331382bffa4fec5cbc57f9ab77908a77ce2687d
Gregory J. Ward <gregoryjward@gmail.com>
2019-10-23 17:16:26 +0000

    Added warning if -oc not set for picture output

--- src/util/dctimestep.c
+++ src/util/dctimestep.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static const char RCSid[] = "$Id: dctimestep.c,v 2.43 2019/10/23 17:00:14 greg Exp $";
+static const char RCSid[] = "$Id: dctimestep.c,v 2.44 2019/10/23 17:16:26 greg Exp $";
 #endif
 /*
  * Compute time-step result using Daylight Coefficient method.
@@ -240,6 +240,10 @@ main(int argc, char *argv[])
 		ofspec = NULL;			/* only need to open once */
 	}
 	if (hasNumberFormat(argv[a])) {		/* generating image(s) */
+		if (outfmt != DTrgbe) {
+			error(WARNING, "changing output type to -oc");
+			outfmt = DTrgbe;
+		}
 		if (ofspec == NULL) {
 			SET_FILE_BINARY(ofp);
 			newheader("RADIANCE", ofp);
@@ -346,9 +350,9 @@ main(int argc, char *argv[])
 	cm_free(cmtx);
 	return(0);
 userr:
-	fprintf(stderr, "Usage: %s [-n nsteps][-o ospec][-i{f|d|h}][-o{f|d}] DCspec [skyf]\n",
+	fprintf(stderr, "Usage: %s [-n nsteps][-o ospec][-i{f|d|h}][-o{f|d|c}] DCspec [skyf]\n",
 				progname);
-	fprintf(stderr, "   or: %s [-n nsteps][-o ospec][-i{f|d|h}][-o{f|d}] Vspec Tbsdf Dmat.dat [skyf]\n",
+	fprintf(stderr, "   or: %s [-n nsteps][-o ospec][-i{f|d|h}][-o{f|d|c}] Vspec Tbsdf Dmat.dat [skyf]\n",
 				progname);
 	return(1);
 }
